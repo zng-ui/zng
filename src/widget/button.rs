85a014aacf5e26f7eb74e0a944a3f123b98189b8
Samuel Guerra <sam.rodr.g@gmail.com>
2019-12-15 17:18:39 -0300

    More ui_widget! design.

--- src/widget/button.rs
+++ src/widget/button.rs
@@ -31,6 +31,8 @@ impl ButtonInput {
     }
 }
 
+/*
+
 // // Declares a button! {} macro.
 // ui_widget! {
 //     // Properties applied to child before calling widget fn.
@@ -157,30 +159,55 @@ ui_widget! {
     use $crate::primitive::*;
 
      // Properties applied to child before calling widget fn.
-     child_properties {
+     properties(child) {
          // Property declaration without default value, if not set does not apply.
          // If set applies margin to child.
-         padding -> margin,
+         padding -> margin;
          // Same with default value.
-         content_align: CENTER -> align,
+         content_align: CENTER -> align;
          // Default value of background_color property that is applied to child.
-         background_color: rgb(0, 0, 0),
+         background_color: rgb(0, 0, 0);
      },
 
 
      // Properties applied to return of widget fn. Same sintax as
      // child_properties.
-     self_properties {
+     properties(self) {
          border: 4., (Var::clone(&text_border), BorderStyle::Dashed);
      },
+
+     on(hover) {
+        background_color: rgb(10, 10, 10);
+        border: 4., (Var::clone(&text_border), BorderStyle::Dashed);
+     }
+
+     on(pressed) {
+
+     }
+
+     on(disabled) {
+
+     }
 
      // widget signature, must name the parameters after child,
      // they behave like required properties in the declared button! macro.
 
-     pub fn button(child: impl Ui, on_click: impl FnMut(ButtonInput, &mut NextUpdate) + 'static) -> impl Ui {
+     pub fn button(child: impl Ui, on_click: impl FnMut(ButtonInput, &mut NextUpdate) + 'static, state: State) -> impl Ui {
          let on_click = Rc::new(RefCell::new(on_click));
          ui! {
              focusable: default;
+             on_mouse_enter: |_, _| {
+                state.hover(true);
+             };
+             on_mouse_leave: |_, _| {
+                state.hover(false);
+             };
+             on_mouse_down: |_, _| {
+                state.pressed(true);
+             };
+             on_mouse_up: |_, _| {
+                state.pressed(false);
+             };
              on_click: enclose! ((on_click) move |ci, n|{
                  if ci.button == MouseButton::Left {
                      (&mut *on_click.borrow_mut())(ButtonInput::Mouse(ci), n);
@@ -254,3 +281,4 @@ pub fn button2(child: impl Ui, on_click: impl FnMut(ButtonInput, &mut NextUpdate
         => child
     }
 }
+*/
