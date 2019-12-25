9bab3c63b7ba512f203a864e08650d10b879b471
Well <well-r@hotmail.com>
2019-12-25 01:10:31 -0300

    more core2 code

--- src/core/core2.rs
+++ src/core/core2.rs
@@ -1,6 +1,7 @@
-use crate::core::{LayoutSize, NextFrame, UiValues};
+use crate::core::{LayoutSize, NextFrame, UiValues, WebRenderEvent};
 use fnv::FnvHashMap;
-use glutin::event::{DeviceEvent, DeviceId, WindowEvent};
+use glutin::event::{DeviceEvent, DeviceId, Event, WindowEvent};
+use glutin::event_loop::{ControlFlow, EventLoop};
 use glutin::window::WindowId;
 use std::any::{Any, TypeId};
 use std::cell::{Cell, Ref, RefCell};
@@ -68,6 +69,28 @@ impl<T> UpdateNotifier<T> {
         }
     }
 }
+
+/// Strong reference to an update channel.
+pub struct StrongUpdateNotifier<T> {
+    note: Rc<UpdateNote<T>>,
+}
+impl<T> StrongUpdateNotifier<T> {
+    pub fn new(is_high_pressure: bool, first_update: T) -> Self {
+        let note = Rc::new(UpdateNote {
+            last_update: RefCell::new(first_update),
+            is_new: Cell::new(true),
+            is_high_pressure,
+        });
+
+        StrongUpdateNotifier{note}
+    }
+    pub fn listener(&self) -> UpdateNotice<T>{
+        UpdateNotice{
+            note: Rc::clone(&self.note)
+        }
+    }
+}
+
 
 /// [UpdateSender] has no linked [UpdateListener] alive so the channel was droped.
 #[derive(Debug)]
@@ -185,7 +208,7 @@ pub trait UiNode: 'static {
     ///
     /// # Arguments
     /// * `available_size`: The total available size for the node. Can contain positive infinity to
-    /// indicate the parent will acomodate any size.
+    /// indicate the parent will accommodate any size.
     ///
     /// # Return
     /// Must return the nodes desired size. Must not contain infinity or NaN.
@@ -215,15 +238,17 @@ pub trait UiNode: 'static {
 
 bitflags! {
     /// What to pump in a Ui tree after an update is applied.
+    #[derive(Default)]
     pub struct UpdateFlags: u8 {
         const UPDATE = 0b0000_0001;
-        const UPD_HP = 0b000_00010;
-        const LAYOUT = 0b000_00100;
-        const RENDER = 0b000_01000;
+        const UPD_HP = 0b0000_0010;
+        const LAYOUT = 0b0000_0100;
+        const RENDER = 0b0000_1000;
     }
 }
 
 /// Global notifier update.
+#[derive(Default)]
 pub struct EventUpdate {
     update: UpdateFlags,
     cleanup: Vec<Box<dyn FnOnce()>>,
@@ -242,6 +267,11 @@ impl EventUpdate {
         modify_update: impl FnOnce(&mut T) + 'static,
     ) -> Result<(), DeadUpdateChannel> {
         if let Some(note) = sender.note.upgrade() {
+            self.update.insert(if note.is_high_pressure {
+                UpdateFlags::UPD_HP
+            } else {
+                UpdateFlags::UPDATE
+            });
             modify_update(&mut *note.last_update.borrow_mut());
             note.is_new.set(true);
             self.cleanup.push(Box::new(move || note.is_new.set(false)));
@@ -324,7 +354,7 @@ impl AppRegister {
 
     pub fn listener<E: EventNotifier>(&self) -> Option<UpdateNotice<E::Args>> {
         if let Some(any) = self.events.get(&TypeId::of::<E>()) {
-            any.downcast_ref::<UpdateNotice<E::Args>>().map(|l| l.clone())
+            any.downcast_ref::<UpdateNotice<E::Args>>().cloned()
         } else {
             None
         }
@@ -370,24 +400,39 @@ pub trait EventNotifier: 'static {
     type Args: 'static;
 }
 
-pub struct MouseEvents {}
+pub struct MouseDownArgs {}
+pub struct MouseDown {}
 
-impl MouseEvents {
-    pub fn new() -> Self {
-        MouseEvents {}
+impl EventNotifier for MouseDown {
+    type Args = MouseDownArgs;
+}
+
+pub struct MouseEvents {
+    mouse_down: StrongUpdateNotifier<MouseDownArgs>
+}
+
+impl Default for MouseEvents {
+    fn default() -> Self {
+        MouseEvents {
+            mouse_down: StrongUpdateNotifier::new(false, MouseDownArgs{})
+        }
     }
 }
 
 impl AppExtension for MouseEvents {
-    fn register(&mut self, r: &mut AppRegister) {}
+    fn register(&mut self, r: &mut AppRegister) {
+        r.register_event::<MouseDown>(self.mouse_down.listener())
+    }
 
-    fn on_window_event(&mut self, window_id: WindowId, event: &WindowEvent, update: &mut EventUpdate) {}
+    fn on_window_event(&mut self, window_id: WindowId, event: &WindowEvent, update: &mut EventUpdate) {
+        //update.notify(sender: &UpdateNotifier<T>, new_update: T)
+    }
 }
 
 pub struct KeyboardEvents {}
 
-impl KeyboardEvents {
-    pub fn new() -> Self {
+impl Default for KeyboardEvents {
+    fn default() -> Self {
         KeyboardEvents {}
     }
 }
@@ -411,7 +456,7 @@ impl<E: AppExtension> App<E> {
     /// Application with default extensions.
     pub fn default() -> App<(MouseEvents, KeyboardEvents)> {
         App {
-            extensions: (MouseEvents::new(), KeyboardEvents::new()),
+            extensions: (MouseEvents::default(), KeyboardEvents::default()),
         }
     }
 
@@ -429,6 +474,46 @@ impl<E: AppExtension> App<E> {
         let mut register = AppRegister::default();
         extensions.register(&mut register);
 
-        todo!()
+        let event_loop = EventLoop::with_user_event();
+        let mut in_event_sequence = false;
+        let mut event_update = EventUpdate::default();
+
+        event_loop.run(move |event, event_loop, control_flow| {
+            *control_flow = ControlFlow::Wait;
+            match event {
+                Event::NewEvents(_) => {
+                    in_event_sequence = true;
+                }
+                Event::EventsCleared => {
+                    in_event_sequence = false;
+                }
+
+                Event::WindowEvent { window_id, event } => {
+                    extensions.on_window_event(window_id, &event, &mut event_update);
+                }
+                Event::UserEvent(WebRenderEvent::NewFrameReady(_window_id)) => {}
+                Event::DeviceEvent { device_id, event } => {
+                    extensions.on_device_event(device_id, &event, &mut event_update);
+                }
+                _ => {}
+            }
+
+            if !in_event_sequence{
+                let updates = event_update.apply();
+
+                if updates.contains(UpdateFlags::UPDATE) {
+                    todo!();
+                }
+                if updates.contains(UpdateFlags::UPD_HP) {
+                    todo!();
+                }
+                if updates.contains(UpdateFlags::LAYOUT) {
+                    todo!();
+                }
+                if updates.contains(UpdateFlags::RENDER) {
+                    todo!();
+                }
+            }
+        })
     }
 }
