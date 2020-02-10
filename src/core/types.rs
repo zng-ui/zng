c442dd523504599549fc09e2335a86fcbb0e0a7d
Samuel Guerra <sam.rodr.g@gmail.com>
2020-02-09 22:16:08 -0300

    Documented rgb/rgba

--- src/core/types.rs
+++ src/core/types.rs
@@ -47,27 +47,48 @@ impl IntoVar<LayoutSideOffsets> for (f32, f32, f32, f32) {
     }
 }
 
+/// Opaque RGB color.
+///
+/// # Arguments
+///
+/// The arguments can either be `f32` in the `0.0..=1.0` range or
+/// `u8` in the `0..=255` range.
+///
+/// # Example
+/// ```
+/// let red = rgb(1.0, 0.0, 0.0);
+/// let green = rgb(0, 255, 0);
+/// ```
 pub fn rgb<C: Into<ColorFComponent>>(r: C, g: C, b: C) -> ColorF {
     rgba(r, g, b, 1.0)
 }
 
+/// RGBA color.
+///
+/// # Arguments
+///
+/// The arguments can either be floating pointer in the `0.0..=1.0` range or
+/// integers in the `0..=255` range.
+///
+/// The rgb arguments must be of the same type, the alpha argument can be of a different type.
+///
+/// # Example
+/// ```
+/// let half_red = rgba(255, 0, 0, 0.5);
+/// let green = rgba(0.0, 1.0, 0.0, 1.0);
+/// let transparent = rgba(0, 0, 0, 0);
+/// ```
 pub fn rgba<C: Into<ColorFComponent>, A: Into<ColorFComponent>>(r: C, g: C, b: C, a: A) -> ColorF {
     ColorF::new(r.into().0, g.into().0, b.into().0, a.into().0)
 }
 
-pub fn default<D: Default>() -> D {
-    D::default()
-}
-
-/// `ColorF` component value.
+/// [rgb] and [rgba] argument conversion helper.
 pub struct ColorFComponent(pub f32);
-
 impl From<f32> for ColorFComponent {
     fn from(f: f32) -> Self {
         ColorFComponent(f)
     }
 }
-
 impl From<u8> for ColorFComponent {
     fn from(u: u8) -> Self {
         ColorFComponent(f32::from(u) / 255.)
@@ -100,6 +121,14 @@ impl IntoVar<Vec<GradientStop>> for Vec<ColorF> {
 }
 
 impl IntoVar<Cow<'static, str>> for &'static str {
+    type Var = OwnedVar<Cow<'static, str>>;
+
+    fn into_var(self) -> Self::Var {
+        OwnedVar(Cow::from(self))
+    }
+}
+
+impl IntoVar<Cow<'static, str>> for String {
     type Var = OwnedVar<Cow<'static, str>>;
 
     fn into_var(self) -> Self::Var {
