564ffa8588706d2778d878c0665cc3d56792785a
Samuel Guerra <sam.rodr.g@gmail.com>
2020-04-05 19:56:43 -0300

    Fixed impl_ui_node documentation.

--- zero-ui-macros/src/lib.rs
+++ zero-ui-macros/src/lib.rs
@@ -16,92 +16,112 @@ pub(crate) mod widget_new;
 
 use widget::CallKind;
 
-/// Generates default implementations of [UiNode](zero_ui::core::UiNode) methods.
+/// Generates default implementations of [`UiNode`](zero_ui::core::UiNode) methods.
 ///
-/// # Usage
+/// # Arguments
 ///
-/// In a inherent impl, annotate the impl block with `#[impl_ui_node(..)]` and custom `UiNode` methods with `#[UiNode]`.
+/// The macro attribute takes arguments that indicate how the missing methods will be generated.
 ///
-/// ```rust
-/// #[impl_ui_node(none)]
-/// impl<C: Var<ColorF>> FillColor<C> {
-///     #[UiNode]
-///     fn render(&self, f: &mut FrameBuilder) {
-///         f.push_fill_color(&LayoutRect::from_size(f.final_size()), *self.color.get_local());
-///     }
+/// * `delegate: {&}, delegate_mut: {&mut}`.
+///
+/// * `delegate_iter: {Iterator<&>}, delegate_iter_mut: {Iterator<&mut>}`.
+///
+/// You can also use shorthand for common delegation:
+///
+/// * `child` is the same as `delegate: &self.child, delegate_mut: &mut self.child`.
+///
+/// * `children` is the same as `delegate_iter: self.children.iter(), delegate_iter_mut: self.children.iter_mut()`.
+///
+/// And for nodes without descendants you can use:
+/// * `none`
+///
+/// # Validation
+/// If delegation is configured but no delegation occurs in the manually implemented methods
+/// you get the error ``"auto impl delegates call to `{}` but this manual impl does not"``.
+///
+/// To disable this error use `#[allow_missing_delegate]` in the method or in the `impl` block.
+///
+/// # Usage Examples
+///
+/// Given an UI node `struct`:
+/// ```
+/// struct FillColor<C> {
+///     color: C,
+///     final_size: LayoutSize,
 /// }
 /// ```
 ///
-/// In a `UiNode` trait impl block, annotate the impl block with `#[impl_ui_node(..)]` and only implement custom methods.
+/// In an `UiNode` trait impl block, annotate the impl block with `#[impl_ui_node(..)]` and only implement custom methods.
 ///
-/// ```rust
+/// ```
 /// #[impl_ui_node(none)]
 /// impl<C: Var<ColorF>> UiNode for FillColor<C> {
 ///     fn render(&self, f: &mut FrameBuilder) {
-///         f.push_fill_color(&LayoutRect::from_size(f.final_size()), *self.color.get_local());
+///         frame.push_color(LayoutRect::from_size(self.final_size), *self.color.get_local());
 ///     }
 /// }
 /// ```
 ///
-/// The generated defaults can be configurated in the macro.
+/// Or, in a inherent impl, annotate the impl block with `#[impl_ui_node(..)]` and custom `UiNode` methods with `#[UiNode]`.
 ///
-/// ## `#[impl_ui_node(none)]`
+/// ```
+/// #[impl_ui_node(none)]
+/// impl<C: LocalVar<ColorF>> FillColor<C> {
+///     pub fn new(color: C) -> Self {
+///         FillColor { color: final_size: LayoutSize::zero() }
+///     }
+///
+///     #[UiNode]
+///     fn render(&self, frame: &mut FrameBuilder) {
+///         frame.push_color(LayoutRect::from_size(self.final_size), *self.color.get_local());
+///     }
+/// }
+/// ```
+///
+/// In both cases a full `UiNode` implement is generated for the node `struct`, but in the second case the inherent methods
+/// are also kept, you can use this to reduce verbosity for nodes with multiple generics.
+///
+/// ## Delegate to `none`
 ///
 /// Generates defaults for UI components without descendants.
 ///
 /// ### Defaults
+///
 /// * Init, Updates: Does nothing, blank implementation.
 /// * Layout: Fills finite spaces, collapses in infinite spaces.
 /// * Render: Does nothing, blank implementation.
 ///
-/// ```rust
-/// # use zero_ui::core::{Var, FrameBuilder, ColorF, LayoutSize, UiValues, NextUpdate};
-/// # pub struct FillColor<C: Var<ColorF>>(C);
-/// #
+/// ```
 /// #[impl_ui_node(none)]
-/// impl<C: Var<ColorF>> FillColor<C> {
+/// impl<C: LocalVar<ColorF>> NoneDelegateSample<C> {
 ///     pub fn new(color: C) -> Self {
-///         FillColor(color)
-///     }
-///     /// Custom impl
-///     #[UiNode]
-///     fn value_changed(&mut self, values: &mut UiValues, update: &mut NextUpdate) {
-///         if self.0.changed() {
-///             update.render_frame();
-///         }
-///     }
-///     /// Custom impl
-///     #[UiNode]
-///     fn render(&self, f: &mut FrameBuilder) {
-///         f.push_fill_color(&LayoutRect::from_size(f.final_size()), *self.color.get_local());
+///          FillColor { color: final_size: LayoutSize::zero() }
 ///     }
 /// }
 /// ```
+///
 /// ### Expands to
 ///
-/// ```rust
-/// impl<C: Value<ColorF>> FillColor<C> {
+/// ```
+/// impl<C: LocalVar<ColorF>> NoneDelegateSample<C> {
 ///     pub fn new(color: C) -> Self {
-///         FillColor(color)
+///          FillColor { color: final_size: LayoutSize::zero() }
 ///     }
 /// }
 ///
-/// impl<C: Value<ColorF>> zero_ui::core::UiNode for FillColor<C> {
-///     /// Custom impl
-///     #[inline]
-///     fn render(&self, f: &mut NextFrame) {
-///         f.push_color(LayoutRect::from_size(f.final_size()), *self.0, None);
-///     }
-///     /// Custom impl
-///     #[inline]
-///     fn value_changed(&mut self, values: &mut UiValues, update: &mut NextUpdate) {
-///         if self.0.changed() {
-///             update.render_frame();
-///         }
-///     }
+/// impl<C: LocalVar<ColorF>> zero_ui::core::UiNode for NoneDelegateSample<C> {
 ///
 ///     #[inline]
-///     fn measure(&mut self, available_size: zero_ui::core::LayoutSize) -> zero_ui::core::LayoutSize {
+///     fn init(&mut self, ctx: &mut zero_ui::core::context::WidgetContext) { }
+///
+///     #[inline]
+///     fn update(&mut self, ctx: &mut zero_ui::core::context::WidgetContext) { }
+///
+///     #[inline]
+///     fn update_hp(&mut self, ctx: &mut zero_ui::core::context::WidgetContext) { }
+///
+///     #[inline]
+///     fn measure(&mut self, available_size: zero_ui::core::types::LayoutSize) -> zero_ui::core::types::LayoutSize {
 ///         let mut size = available_size;
 ///         if size.width.is_infinite() {
 ///             size.width = 0.0;
@@ -111,123 +131,171 @@ use widget::CallKind;
 ///         }
 ///         size
 ///     }
+///
 ///     #[inline]
-///     fn point_over(&self, hits: &Hits) -> Option<LayoutPoint> {
-///         None
-///     }
+///     fn arrange(&mut self, final_size: zero_ui::core::types::LayoutSize) { }
+///
 ///     #[inline]
-///     fn init(&mut self, values: &mut zero_ui::core::UiValues, update: &mut zero_ui::core::NextUpdate) {}
+///     fn render(&self, frame: &mut zero_ui::core::render::FrameBuilder) { }
+///
 ///     #[inline]
-///     fn arrange(&mut self, final_size: zero_ui::core::LayoutSize) {}
-///     #[inline]
-///     fn keyboard_input(
-///         &mut self,
-///         input: &zero_ui::core::KeyboardInput,
-///         values: &mut zero_ui::core::UiValues,
-///         update: &mut zero_ui::core::NextUpdate,
-///     ) {
-///     }
-///     #[inline]
-///     fn window_focused(
-///         &mut self,
-///         focused: bool,
-///         values: &mut zero_ui::core::UiValues,
-///         update: &mut zero_ui::core::NextUpdate,
-///     ) {
-///     }
-///     #[inline]
-///     fn mouse_input(
-///         &mut self,
-///         input: &zero_ui::core::MouseInput,
-///         hits: &zero_ui::core::Hits,
-///         values: &mut zero_ui::core::UiValues,
-///         update: &mut zero_ui::core::NextUpdate,
-///     ) {
-///     }
-///     #[inline]
-///     fn mouse_move(
-///         &mut self,
-///         input: &zero_ui::core::UiMouseMove,
-///         hits: &zero_ui::core::Hits,
-///         values: &mut zero_ui::core::UiValues,
-///         update: &mut zero_ui::core::NextUpdate,
-///     ) {
-///     }
-///     #[inline]
-///     fn mouse_entered(&mut self, values: &mut zero_ui::core::UiValues, update: &mut zero_ui::core::NextUpdate) {}
-///     #[inline]
-///     fn mouse_left(&mut self, values: &mut zero_ui::core::UiValues, update: &mut zero_ui::core::NextUpdate) {}
-///     #[inline]
-///     fn close_request(&mut self, values: &mut zero_ui::core::UiValues, update: &mut zero_ui::core::NextUpdate) {}
-///     #[inline]
-///     fn parent_value_changed(
-///         &mut self,
-///         values: &mut zero_ui::core::UiValues,
-///         update: &mut zero_ui::core::NextUpdate,
-///     ) {
-///     }
+///     fn deinit(&mut self, ctx: &mut zero_ui::core::context::WidgetContext) { }
+///
 /// }
 /// ```
 ///
-/// ## `#[impl_ui_node(child)]`
+/// ## Delegate to one (`child` or `delegate, delegate_mut`)
 ///
-/// Shorthand for:
-/// ```rust
-/// #[impl_ui_node(
-///     delegate: &self.child,
-///     delegate_mut: &mut self.child
-/// )]
-/// ```
-///
-/// ## `#[impl_ui_node(children)]`
-///
-/// Shorthand for:
-/// ```rust
-/// #[impl_ui_node(
-///     delegate_iter: self.children.iter(),
-///     delegate_iter_mut: mut self.children.iter_mut()
-/// )]
-/// ```
-///
-/// ## `#[impl_ui_node(delegate: expr, delegate_mut: expr)]`
-///
-/// Generates defaults by delegating the method calls to
-/// a reference of another UiNode component.
-///
-/// Both arguments are required and in order.
-///
-/// ```rust
-/// #[impl_ui_node(delegate: self.0.borrow(), delegate_mut: self.0.borrow_mut())]
-/// // TODO
-/// ```
-///
-/// ## `#[impl_ui_node(delegate_iter: expr, delegate_iter_mut: expr)]`
-///
-/// Generates defaults by delegating the method calls to
-/// all UiNode component references provided by the iterators.
+/// Generates defaults for UI components with a single child node. This is the most common mode,
+/// used by property nodes.
 ///
 /// ### Defaults
-/// * Events: Calls the same event method for each `UiNode` delegate provided by the iterator.
-/// * Layout: Measure all delegates the largest size is returned. Arranges all delegates with the default top-left alignment.
-/// * Render: Renders all delegates on top of each other in the iterator order.
-/// * Hit-test: Returns the first delegate hit or `None` if none hit.
 ///
-/// ```rust
-/// #[impl_ui_node(delegate_iter: self.0.iter(), delegate_iter_mut: self.0.iter_mut())]
-/// // TODO
+/// * Init, Updates: Delegates to same method in child.
+/// * Layout: Is the same size as child.
+/// * Render: Delegates to child render.
+///
+/// ```
+/// #[impl_ui_node(child)]
+/// impl<C: UiNode> UiNode for ChildDelegateSample<C> { }
 /// ```
 ///
-/// ## Delegate Validation
-/// If delegation is configured but no delegation occurs in the manually implemented methods
-/// you get the error `"auto impl delegates call to `{}` but this manual impl does not"`.
+/// ### Expands to
 ///
-/// To disable this error use `#[allow_missing_delegate]` in the method or in the `impl` block.
+/// ```
+/// impl<C: UiNode> UiNode ChildDelegateSample<C> {
+///
+///     #[inline]
+///     fn init(&mut self, ctx: &mut zero_ui::core::context::WidgetContext) {
+///         let child = { &mut self.child };
+///         child.init(ctx)
+///     }
+///
+///     #[inline]
+///     fn update(&mut self, ctx: &mut zero_ui::core::context::WidgetContext) {
+///         let child = { &mut self.child };
+///         child.update(ctx)
+///     }
+///
+///     #[inline]
+///     fn update_hp(&mut self, ctx: &mut zero_ui::core::context::WidgetContext) {
+///         let child = { &mut self.child };
+///         child.update_hp(ctx)
+///     }
+///
+///     #[inline]
+///     fn measure(&mut self, available_size: zero_ui::core::types::LayoutSize) -> zero_ui::core::types::LayoutSize {
+///         let child = { &mut self.child };
+///         child.measure(available_size)
+///     }
+///
+///     #[inline]
+///     fn arrange(&mut self, final_size: zero_ui::core::types::LayoutSize) {
+///         let child = { &mut self.child };
+///         child.arrange(final_size)
+///     }
+///
+///     #[inline]
+///     fn render(&self, frame: &mut zero_ui::core::render::FrameBuilder) {
+///         let child = { &self.child };
+///         child.render(frame)
+///     }
+///
+///     #[inline]
+///     fn deinit(&mut self, ctx: &mut zero_ui::core::context::WidgetContext) {
+///         let child = { &mut self.child };
+///         child.deinit(ctx)
+///     }
+///
+/// }
+/// ```
+///
+/// ## Delegate to many (`children` or `delegate_iter, delegate_iter_mut`)
+///
+/// Generates defaults for UI components with a multiple children nodes. This is used mostly by
+/// layout panels.
+///
+/// ### Defaults
+///
+/// * Init, Updates: Delegates to same method in each child.
+/// * Layout: Is the same size as the largest child.
+/// * Render: Z-stacks the children. Last child on top.
+///
+/// ```
+/// #[impl_ui_node(children)]
+/// impl<C: UiNode> UiNode for ChildrenDelegateSample<C> { }
+/// ```
+///
+/// ### Expands to
+///
+/// ```
+/// impl<C: UiNode> UiNode ChildrenDelegateSample<C> {
+///
+///     #[inline]
+///     fn init(&mut self, ctx: &mut zero_ui::core::context::WidgetContext) {
+///         for child in { self.child.iter_mut() } {
+///             child.init(ctx)
+///         }
+///     }
+///
+///     #[inline]
+///     fn update(&mut self, ctx: &mut zero_ui::core::context::WidgetContext) {
+///         for child in { self.child.iter_mut() } {
+///             child.update(ctx)
+///         }
+///     }
+///
+///     #[inline]
+///     fn update_hp(&mut self, ctx: &mut zero_ui::core::context::WidgetContext) {
+///         for child in { self.child.iter_mut() } {
+///             child.update_hp(ctx)
+///         }
+///     }
+///
+///     #[inline]
+///     fn measure(&mut self, available_size: zero_ui::core::types::LayoutSize) -> zero_ui::core::types::LayoutSize {
+///         let mut size = Default::default();
+///         for child in { self.child.iter_mut() } {
+///            size = child.measure(available_size).max(size);
+///         }
+///         size
+///     }
+///
+///     #[inline]
+///     fn arrange(&mut self, final_size: zero_ui::core::types::LayoutSize) {
+///         for child in { self.child.iter_mut() } {
+///             child.arrange(final_size)
+///         }
+///     }
+///
+///     #[inline]
+///     fn render(&self, frame: &mut zero_ui::core::render::FrameBuilder) {
+///         for child in { self.child.iter() } {
+///             child.render(frame)
+///         }
+///     }
+///
+///     #[inline]
+///     fn deinit(&mut self, ctx: &mut zero_ui::core::context::WidgetContext) {
+///         for child in { self.child.iter_mut() } {
+///             child.deinit(ctx)
+///         }
+///     }
+///
+/// }
+/// ```
 #[proc_macro_attribute]
 pub fn impl_ui_node(args: TokenStream, input: TokenStream) -> TokenStream {
     impl_ui_node::gen_impl_ui_node(args, input)
 }
 
 /// Declares a new widget property.
+///
+///
+///
+/// # Usage
+///
+/// Annotate
 #[proc_macro_attribute]
 pub fn property(args: TokenStream, input: TokenStream) -> TokenStream {
     property::expand_property(args, input)
