7f114d0ecde8cba7f09677c7e518980a07d1b474
Samuel Guerra <sam.rodr.g@gmail.com>
2020-07-11 16:26:57 -0300

    Implemented is_focus_within.

--- src/properties/is_state_.rs
+++ src/properties/is_state_.rs
@@ -6,17 +6,6 @@ use crate::core::var::{ObjVar, StateVar, VarIsReadOnly};
 use crate::core::UiNode;
 use crate::core::{impl_ui_node, property};
 
-trait ResultVarReadOnlyExt {
-    fn warn_err(self, property: &str);
-}
-impl ResultVarReadOnlyExt for Result<(), VarIsReadOnly> {
-    fn warn_err(self, property: &str) {
-        if self.is_err() {
-            error_println!("cannot update `{}` because it is set to a read-only var", property)
-        }
-    }
-}
-
 struct IsHovered<C: UiNode> {
     child: C,
     state: StateVar,
@@ -37,10 +26,10 @@ impl<C: UiNode> UiNode for IsHovered<C> {
 
         if *self.state.get(ctx.vars) {
             if self.mouse_leave.updates(ctx.events).iter().any(|a| a.concerns_widget(ctx)) {
-                ctx.updates.push_set(&self.state, false, ctx.vars).warn_err("is_hovered");
+                ctx.updates.push_set(&self.state, false, ctx.vars).expect("is_hovered");
             }
         } else if self.mouse_enter.updates(ctx.events).iter().any(|a| a.concerns_widget(ctx)) {
-            ctx.updates.push_set(&self.state, true, ctx.vars).warn_err("is_hovered");
+            ctx.updates.push_set(&self.state, true, ctx.vars).expect("is_hovered");
         }
     }
 }
@@ -76,11 +65,11 @@ impl<C: UiNode> UiNode for IsPressed<C> {
         if *self.state.get(ctx.vars) {
             if self.mouse_up.has_updates(ctx.events) {
                 // if mouse_up in any place.
-                ctx.updates.push_set(&self.state, false, ctx.vars).warn_err("is_pressed");
+                ctx.updates.push_set(&self.state, false, ctx.vars).expect("is_pressed");
             }
         } else if self.mouse_down.updates(ctx.events).iter().any(|a| a.concerns_widget(ctx)) {
             // if not pressed and mouse down inside.
-            ctx.updates.push_set(&self.state, true, ctx.vars).warn_err("is_pressed");
+            ctx.updates.push_set(&self.state, true, ctx.vars).expect("is_pressed");
         }
     }
 }
@@ -114,16 +103,58 @@ impl<C: UiNode> UiNode for IsFocused<C> {
             let is_focused = u.new_focus.as_ref().map(|p| p.widget_id() == ctx.widget_id).unwrap_or_default();
 
             if was_focused != is_focused {
-                self.state.push_set(is_focused, ctx.vars, ctx.updates).warn_err("is_focused");
+                self.state.push_set(is_focused, ctx.vars, ctx.updates).expect("is_focused");
             }
         }
         self.child.update(ctx);
     }
 }
 
+/// If the widget has keyboard focus.
+///
+/// This is only `true` if the widget itself is focused.
+/// You can use [`is_focus_within`] to check if the focused widget is within this one.
 #[property(context)]
 pub fn is_focused(child: impl UiNode, state: StateVar) -> impl UiNode {
     IsFocused {
+        child,
+        state,
+        focus_changed: EventListener::never(false),
+    }
+}
+
+struct IsFocusWithin<C: UiNode> {
+    child: C,
+    state: StateVar,
+    focus_changed: EventListener<FocusChangedArgs>
+}
+
+#[impl_ui_node(child)]
+impl<C: UiNode> UiNode for IsFocusWithin<C> {
+    fn init(&mut self, ctx: &mut WidgetContext) {
+        self.focus_changed = ctx.events.listen::<FocusChanged>();
+        self.child.init(ctx);
+    }
+
+    fn update(&mut self, ctx: &mut WidgetContext) {
+        if let Some(u) = self.focus_changed.updates(ctx.events).last() {
+            let was_focused = *self.state.get(ctx.vars);
+            let is_focused = u.new_focus.as_ref().map(|p| p.contains(ctx.widget_id)).unwrap_or_default();
+
+            if was_focused != is_focused {
+                self.state.push_set(is_focused, ctx.vars, ctx.updates).expect("is_focus_within");
+            }
+        }
+        self.child.update(ctx);
+    }
+}
+
+/// If the widget or one of its descendants has keyboard focus.
+///
+/// To check if only the widget has keyboard focus use [`is_focused`](is_focused).
+#[property(context)]
+pub fn is_focus_within(child: impl UiNode, state: StateVar) -> impl UiNode {
+    IsFocusWithin {
         child,
         state,
         focus_changed: EventListener::never(false),
