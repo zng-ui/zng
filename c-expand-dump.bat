c39a811087ecb9fd95426f05109eb2903e3cddd9
Samuel Guerra <sam.rodr.g@gmail.com>
2020-07-29 20:00:16 -0300

    Better expand script input.

--- c-expand-dump.bat
+++ c-expand-dump.bat
@@ -3,6 +3,6 @@ IF [%1]==[] (
     cargo +nightly rustc --profile=check -- -Zunstable-options --pretty=expanded > dump.rs
     @echo expand ^*.rs ^> dump.rs
 ) ELSE (
-    cargo +nightly rustc  --example %* --profile=check -- -Zunstable-options --pretty=expanded > dump.rs
-    @echo expand %*.rs ^> dump.rs
+    cargo +nightly rustc  --example %1 --profile=check -- -Zunstable-options --pretty=expanded > dump.rs
+    @echo expand %1.rs ^> dump.rs
 )
