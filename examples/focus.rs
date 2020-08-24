3940fb298c34430eece7ef7bc4fa8752864be3ac
Samuel Guerra <sam.rodr.g@gmail.com>
2020-08-24 18:54:34 -0300

    More focus examples.

--- examples/focus.rs
+++ examples/focus.rs
@@ -6,24 +6,47 @@ fn main() {
         window! {
             title: "Focus Example";
             on_focus_changed: |a| {
-                fn id(path: &Option<WidgetPath>) -> Option<WidgetId> {
-                    path.as_ref().map(|p|p.widget_id())
+                fn id(path: &Option<WidgetPath>) -> String {
+                    path.as_ref().map(|p|format!("{:?}", p.widget_id())).unwrap_or_else(||"None".to_owned())
                 }
                 let args = a.args();
-                println!("focus changed: {:?} -> {:?}", id(&args.prev_focus), id(&args.new_focus));
+                println!("focus changed: {} -> {}", id(&args.prev_focus), id(&args.new_focus));
             };
+            content_align: unset!;
             content: v_stack! {
-                spacing: 5.0;
                 items: ui_vec![
-                    example("Button 5", TabIndex(5)),
-                    example("Button 4", TabIndex(3)),
-                    example("Button 3", TabIndex(2)),
-                    example("Button 1", TabIndex(0)),
-                    example("Button 2", TabIndex(0)),
+                    h_stack! {
+                        alt_focus_scope: true;
+                        spacing: 5.0;
+                        margin: 5.0;
+                        items: ui_vec![
+                            example("alt", TabIndex::AUTO),
+                            example("scope", TabIndex::AUTO),
+                        ];
+                    },
+                    v_stack! {
+                        focus_scope: true;
+                        focus_shortcut: shortcut!(T);
+                        margin: (50.0, 0.0, 0.0, 0.0);
+                        align: Alignment::CENTER;
+                        spacing: 5.0;
+                        items: ui_vec![
+                            text! { text: "TabIndex (T)"; font_weight: FontWeight::BOLD; align: Alignment::CENTER; },
+                            tab_index_btn(TabIndex(5)),
+                            tab_index_btn(TabIndex(3)),
+                            tab_index_btn(TabIndex(2)),
+                            tab_index_btn(TabIndex(0)),
+                            tab_index_btn(TabIndex(0)),
+                        ];
+                    }
                 ];
             };
         }
     })
+}
+
+fn tab_index_btn(tab_index: TabIndex) -> impl Widget {
+    example(format!("{:?}", tab_index), tab_index)
 }
 
 fn example(content: impl Into<Text>, tab_index: TabIndex) -> impl Widget {
