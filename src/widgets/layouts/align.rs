1fd489461989e2d800f8df380e18c0595e37aa3b
Samuel Guerra <sam.rodr.g@gmail.com>
2020-09-29 15:44:25 -0300

    Refactored center to be an actual widget.

--- src/widgets/layouts/align.rs
+++ src/widgets/layouts/align.rs
@@ -1,8 +1,21 @@
-use crate::core::{units::Alignment, UiNode};
-use crate::properties::align;
+use crate::core::{units::Alignment, widget, UiNode, Widget};
+use crate::properties::{align, capture_only::widget_child};
+
+widget! {
+    center;
+
+    default_child {
+        child -> widget_child: required!;
+    }
+
+    #[inline]
+    fn new_child(child) -> impl UiNode {
+        align::set(child.unwrap(), Alignment::CENTER)
+    }
+}
 
 /// Centralizes the node.
 #[inline]
-pub fn center(child: impl UiNode) -> impl UiNode {
-    align::set(child, Alignment::CENTER)
+pub fn center(child: impl UiNode) -> impl Widget {
+    center! { child; }
 }
