4c68d53270afa7f2192f647261b0cc83759714ef
Samuel Guerra <sam.rodr.g@gmail.com>
2020-10-30 18:59:18 -0300

    Fixed uniform_grid module.

--- src/widgets/layouts.rs
+++ src/widgets/layouts.rs
@@ -3,9 +3,9 @@
 mod align;
 //mod grid; TODO
 mod stacks;
-mod uniform_grid;
+mod uniform_grid_;
 
 pub use align::*;
 //pub use grid::*;
 pub use stacks::*;
-pub use uniform_grid::*;
+pub use uniform_grid_::*;
