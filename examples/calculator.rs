0995e75bf3a815d2e44ca2b909bd23d7756bbe14
Samuel Guerra <sam.rodr.g@gmail.com>
2020-11-06 01:07:08 -0300

    Refactored calculator to have state in single var.

--- examples/calculator.rs
+++ examples/calculator.rs
@@ -1,10 +1,11 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use meval::eval_str;
+use enclose::enclose;
 use zero_ui::prelude::*;
 
 fn main() {
     App::default().run_window(|_| {
-        let buffer = var("");
+        let calc: RcVar<Calculator> = var(Calculator::default());
         window! {
             title: "Calculator";
             resizable: false;
@@ -12,20 +13,35 @@ fn main() {
                 spacing: 5;
                 items: ui_vec![
                     text! {
-                        text: buffer.clone();
+                        text: calc.map(|c| c.result());
                         align: Alignment::RIGHT;
                         font_size: 32.pt();
+                        color: calc.map(|c| c.color());
                     },
-                    controls(buffer)
+                    controls(calc.clone())
                 ];
+            };
+            on_char_input: enclose! { (calc) move |ctx, args| {
+                let char_ = args.character;
+                calc.modify(ctx.vars, move |c|c.push(char_));
+            }};
+            // on_enter
+            on_preview_key_down: move |ctx, args| {
+                match args.key {
+                    Some(VirtualKeyCode::Return) | Some(VirtualKeyCode::NumpadEnter) => {
+                        calc.modify(ctx.vars, |c|c.eval());
+                        args.stop_propagation();
+                    },
+                    _ => { }
+                }
             };
         }
     })
 }
 
-fn controls(buffer: RcVar<Text>) -> impl Widget {
-    let b = |c| btn(buffer.clone(), c);
-    let btn_eq = btn_eval(buffer.clone());
+fn controls(calc: RcVar<Calculator>) -> impl Widget {
+    let b = |c| btn(calc.clone(), c);
+    let btn_eq = btn_eval(calc.clone());
     uniform_grid! {
         spacing: 2;
         columns: 4;
@@ -38,27 +54,96 @@ fn controls(buffer: RcVar<Text>) -> impl Widget {
     }
 }
 
-fn btn(buffer: RcVar<Text>, c: char) -> impl Widget {
+fn btn(calc: RcVar<Calculator>, c: char) -> impl Widget {
     button! {
         on_click: move |ctx, _| {
-            buffer.modify(ctx.vars, move |b| b.to_mut().push(c))
+            calc.modify(ctx.vars, move |b| b.push(c))
         };
         content: text(c.to_string());
     }
 }
 
-fn btn_eval(buffer: RcVar<Text>) -> impl Widget {
+fn btn_eval(calc: RcVar<Calculator>) -> impl Widget {
     button! {
-        on_click: move |ctx, _| {
-            buffer.modify(ctx.vars, move |b| eval(b.to_mut()))
-        };
+        on_click: move |ctx, _| calc.modify(ctx.vars, |c|c.eval());
         content: text("=");
     }
 }
 
-fn eval(buffer: &mut String) {
-    use std::fmt::Write;
-    let eval_result = eval_str(&buffer).unwrap_or(0.0);
-    buffer.clear();
-    let _ = write!(buffer, "{}", eval_result);
+#[derive(Default, Clone, Debug)]
+struct Calculator {
+    buffer: String,
+    error: bool,
+}
+impl Calculator {
+    pub fn result(&self) -> Text {
+        if self.buffer.is_empty() {
+            "0".into()
+        } else {
+            self.buffer.clone().into()
+        }
+    }
+
+    pub fn color(&self) -> Rgba {
+        if self.error {
+            web_colors::RED
+        } else {
+            web_colors::WHITE
+        }
+    }
+
+    fn char_is_valid(c: char) -> bool {
+        c.is_digit(10) || ['.', '+', '-', '*', '/'].contains(&c)
+    }
+
+    pub fn push(&mut self, c: char) {
+        if !Self::char_is_valid(c) {
+            return;
+        }
+
+        if self.error {
+            self.buffer.clear();
+            self.error = false;
+        }
+
+        if self.buffer.is_empty() && c == '.' {
+            self.buffer.push_str("0.");
+        } else {
+            if !c.is_digit(10) && self.trailing_op() {
+                self.buffer.pop();
+            }
+
+            self.buffer.push(c);
+        }
+    }
+
+    fn trailing_op(&self) -> bool {
+        self.buffer.chars().last().map(|c| !c.is_digit(10)).unwrap_or(false)
+    }
+
+    pub fn eval(&mut self) {
+        use std::fmt::Write;
+
+        let expr = if self.trailing_op() {
+            &self.buffer[..self.buffer.len() - 1]
+        } else {
+            &self.buffer
+        };
+
+        if expr.is_empty() {
+            self.buffer.clear();
+            self.error = false;
+        } else {
+            match eval_str(expr) {
+                Ok(new) => {
+                    self.buffer.clear();
+                    let _ = write!(&mut self.buffer, "{}", new);
+                    self.error = false;
+                }
+                Err(_) => {
+                    self.error = true;
+                }
+            }
+        }
+    }
 }
