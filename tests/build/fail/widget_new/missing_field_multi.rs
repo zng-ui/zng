de8510134d273fe2bb037337593f6d0e38019535
Samuel Guerra <sam.rodr.g@gmail.com>
2021-03-04 18:52:54 -0300

    Test missing field in named property input.

---
+++ tests/build/fail/widget_new/missing_field_multi.rs
@@ -0,0 +1,14 @@
+use zero_ui::core::units::*;
+use zero_ui::properties::background::background_gradient;
+use zero_ui::widgets::blank;
+
+fn main() {
+    let _ = blank! {
+        // background_gradient has two fields
+        // the error highlights the property
+        // in a struct initializer the struct name is highlighted
+        background_gradient = {
+            axis: 0.deg(),
+        }
+    };
+}
