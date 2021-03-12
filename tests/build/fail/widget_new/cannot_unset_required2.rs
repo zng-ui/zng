4f46550ef8ecab2a34941ac37cf41a039f539a76
Samuel Guerra <sam.rodr.g@gmail.com>
2021-03-11 22:12:49 -0300

    Tested error for trying to unset a required property.

---
+++ tests/build/fail/widget_new/cannot_unset_required2.rs
@@ -0,0 +1,21 @@
+use zero_ui::core::widget;
+
+#[widget($crate::test_widget)]
+pub mod test_widget {
+    use zero_ui::core::NilUiNode;
+
+    properties! {
+        foo: bool;
+    }
+
+    fn new_child(foo: bool) -> NilUiNode {
+        println!("{}", foo);
+        NilUiNode
+    }
+}
+
+fn main() {
+    let _ = test_widget! {
+        foo = unset!;
+    };
+}
