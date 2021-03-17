83ab81819c7c4f5dae5831d81e26777815092844
Samuel Guerra <sam.rodr.g@gmail.com>
2021-03-17 00:29:34 -0300

    Added test for unknown member in when expr.

---
+++ tests/build/fail/widget_new/unknown_member_in_when_expr1.rs
@@ -0,0 +1,11 @@
+use zero_ui::properties::{margin, states::is_pressed};
+use zero_ui::widgets::blank;
+
+fn main() {
+    let _ = blank! {
+        margin = 0;
+        when self.is_pressed.1 { // only .0 or .state allowed.
+            margin = 1;
+        }
+    };
+}
