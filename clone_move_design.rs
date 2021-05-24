252bdfcf4ab43d248d512f3e554a23263def6181
Samuel Guerra <sam.rodr.g@gmail.com>
2021-05-24 14:02:37 -0300

    More clone_move design.

--- clone_move_design.rs
+++ clone_move_design.rs
@@ -16,7 +16,56 @@ fn main() {
         clone_move! { |ctx, args| 
             let wns = ctx.services.req::<Windows>();
             wns.window(args.window_id);
+            let hello = "";
+            // can't known hello is not from the outside.
+            println!("{} {}", hello.clone(), other);
         }
+    );
+
+    // C
+    foo(
+        #[clone_move(hello, |ctx, args|)] {
+            // `hello` is cloned before creating the closure, the clone is moved in,
+            // `other` is moved in.
+            println!("{} {}", hello, other);
+
+            // this works in stable, downside is that rust-analyzer does not autocomplete for `|ctx, args|`
+            // and rustfmt moves the attribute above the block.
+        }
+    );
+
+    // D
+    foo(
+        clone_move! { hello, .. |ctx, args|
+            // closure block
+        }
+    );
+
+    // manual
+    foo(
+        {
+            let hello = hello.clone();
+            move |ctx, args| {
+
+            }
+        }
+    );
+
+    // manual assisted
+    foo(
+        {
+            clone!(hello, ..);// create another hello, etc.
+            move |ctx, args| {
+                
+            }
+        }
+    );
+
+    // current
+    foo(
+        enclose!((hello) move |ctx, args| {
+            println!("{} {}", hello, other);
+        })
     );
 
     // this code should compile.
