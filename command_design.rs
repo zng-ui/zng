94ffe60bb9169515e906fc6f338091e804e0794e
Samuel Guerra <sam.rodr.g@gmail.com>
2021-05-27 01:05:05 -0300

    More command design work.

--- command_design.rs
+++ command_design.rs
@@ -5,6 +5,8 @@ mod declare_stuff{
   trait Command: 'static {
     fn enabled() -> Var<bool> {}
     fn visible() -> Var<bool> {}
+    fn text() -> Var<Text> {}
+    fn shortcuts() -> Var<Shortcuts> {}
   }
 
   struct Commands {}
@@ -12,10 +14,8 @@ mod declare_stuff{
     fn execute<C: Command>(&mut self, command: C) {
       // emits command event
     }
-    fn enabled<C: Command>(&mut self, command: C) -> Var<bool> {
 
-    }
-    fn visible<C: Command>(&mut self, command: C) -> Var<bool> {
+    fn on_shortcut_collision(handler: Fn(MultipleCommandsSameShortcut)) {
 
     }
   }  
@@ -57,12 +57,54 @@ mod send_stuff {
 // 
 // How to use (Receive)
 //
+#[property(event)]
+pub fn on_copy(child: impl UiNode, can_run: impl FnMut(&mut WidgetContext) -> bool, run: impl FnMut(&mut WidgetContext, CommandArgs)) -> impl UiNode {
+  struct OnCopyNode<C, Q, R> {
+    child: C,
+    can_run: Q,
+    run: R,
+    handler: CommandHandler,
+  }
+  impl UiNode for OnCopyNode {
+    fn init(&mut self, ctx: &mut WidgetContext) {
+      self.handler = ctx.services.req::<Commands>().handler::<CopyCommand>();
+      if self.can_run(ctx) {
+        self.handler.set(true);
+      }
+    }
 
+    fn update(&mut self, ctx: &mut WidgetContext) {
+      if self.can_run(ctx) {
+        if self.handler.run_requested() {
+          self.run(ctx);
+        }
+      } else {
+        self.handler.set(false);
+      }
+    }
+  }
+}
 
+#[widget($crate::text_box)]
+pub mod text_box {
+  properties! {
+    on_copy = {
+      can_run: |ctx| IsEnabled::get(ctx.vars) && IsVisible::get(ctx.vars),
+      run: |ctx| {
+        let text = ctx.widget_state[TextBoxTextState];
+        ctx.services.req::<Clipboard>().set_text(text);
+      }
+    };
+  }
+}
 
 
 // 
 // How to bind shortcuts
 //
 
-
+fn main() {
+  App::default().run(|ctx| {
+    CopyCommand::shortcuts().set(ctx.vars, shortcut![Ctrl+C]);
+  })
+}
