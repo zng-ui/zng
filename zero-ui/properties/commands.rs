b527f5684f86f95d6b30173f9cb7f4f6fce9cbea
Samuel Guerra <sam.rodr.g@gmail.com>
2021-06-22 12:31:48 -0300

    Added more commands docs.

--- zero-ui/properties/commands.rs
+++ zero-ui/properties/commands.rs
@@ -1,4 +1,18 @@
-//! Common commands
+//! Common commands.
+//!
+//! Commands are special events that represent app actions. They do not implement the action invocation or fulfillment, but
+//! allows for *binding states* between a widget that fulfills the command and a widget that invokes the command. For
+//! example, the [`CopyCommand`] represents the action of "placing a copy of the selection in the clipboard", a widget can implement
+//! this command by handling its event using TODO, a different widget can invoke the command using [`CopyCommand::notify`] and can bind 
+//! its enabled property to [`CopyCommand::enabled`]. The widgets **don't need to known each other** when the first widget can copy 
+//! it enables the command, this in turn enables the second widget that will invoke the command on an user interaction.
+//!
+//! Commands can also have any number of metadata associated with then, this metadata is implemented using extension traits,
+//! [`CommandNameExt`] adds a `name` text that can be user as a button content for example, [`CommandInfoExt`] adds a longer
+//! `info` text that can be used as a *tool-tip*. Some metadata enable new forms of interaction, [`CommandShortcutExt`] adds
+//! a `shortcut` value **and** causes the [`GestureManager`](crate::core::gesture::GestureManager) to start invoking enabled 
+//! commands when the shortcut is pressed.
+
 use crate::core::command::*;
 
 command! {
