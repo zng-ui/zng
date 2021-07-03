f7bd175fc9171cf3b7539fa8f210e55a85e14e37
Samuel Guerra <sam.rodr.g@gmail.com>
2021-07-02 23:19:04 -0300

    Fixed property header script reinserting elements removed by another script;

--- zero-ui-proc-macros/src/js/property_header.js
+++ zero-ui-proc-macros/src/js/property_header.js
@@ -13,39 +13,45 @@ function move_properties() {
     if (functions !== null) {
         // we are in the functions list page:
 
-        // create section header
-        let pf = document.createElement('h2');
-        pf.id = 'property-functions';
-        pf.classList.add('section-header');
-        let a = document.createElement('a');
-        a.href = '#' + pf.id;
-        a.innerHTML = 'Property Functions';
-        pf.appendChild(a);
-        functions.parentNode.insertBefore(pf, functions);
-
-        // create section link in the sidebar
-        let ul = document.querySelector('div.block.items ul');
-        let side_functions = ul.querySelector('a[href=\u0022#functions\u0022]').parentNode;
-        let li = document.createElement('li');
-        li.appendChild(a.cloneNode(true));
-        ul.insertBefore(li, side_functions);
-
-        // create table
-        let table = document.createElement('table');
-        table.style = 'display:block;';
-        let tbody = document.createElement('tbody');
-        document.property_fns.forEach(function(tr) {
-            tbody.appendChild(tr);
+        let property_fns = document.property_fns.filter(function(tr) {
+            (document.documentElement.contains(tr))
         });
         document.property_fns = null;
-        table.appendChild(tbody);
-        functions.parentNode.insertBefore(table, pf.nextSibling);
 
-        // remove empty section
-        if (functions.nextElementSibling.querySelector('tr') === null) {
-            functions.nextElementSibling.remove();
-            functions.remove();
-            side_functions.remove();
+        if (property_fns.length > 0) {
+            // create section header
+            let pf = document.createElement('h2');
+            pf.id = 'property-functions';
+            pf.classList.add('section-header');
+            let a = document.createElement('a');
+            a.href = '#' + pf.id;
+            a.innerHTML = 'Property Functions';
+            pf.appendChild(a);
+            functions.parentNode.insertBefore(pf, functions);
+
+            // create section link in the sidebar
+            let ul = document.querySelector('div.block.items ul');
+            let side_functions = ul.querySelector('a[href=\u0022#functions\u0022]').parentNode;
+            let li = document.createElement('li');
+            li.appendChild(a.cloneNode(true));
+            ul.insertBefore(li, side_functions);
+
+            // create table
+            let table = document.createElement('table');
+            table.style = 'display:block;';
+            let tbody = document.createElement('tbody');
+            property_fns.forEach(function(tr) {
+                tbody.appendChild(tr);
+            });
+            table.appendChild(tbody);
+            functions.parentNode.insertBefore(table, pf.nextSibling);
+
+            // remove empty section
+            if (functions.nextElementSibling.querySelector('tr') === null) {
+                functions.nextElementSibling.remove();
+                functions.remove();
+                side_functions.remove();
+            }
         }
     }
 
