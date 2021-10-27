189f59b3fcecc0a7259f0efc60f07f9068789b55
Samuel Guerra <sam.rodr.g@gmail.com>
2021-10-27 20:52:12 -0300

    Fixed prebuild rebuilding every run.

--- zero-ui-view-prebuilt/build.rs
+++ zero-ui-view-prebuilt/build.rs
@@ -2,7 +2,6 @@ use std::{env, path::Path};
 
 fn main() {
     let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    println!("cargo:rerun-if-changed=lib/*.lib");
-    println!("cargo:rerun-if-changed=lib/*.a");
-    println!("cargo:rustc-link-search={}", Path::new(&dir).join("lib").display());
+    let lib = Path::new(&dir).join("lib");
+    println!("cargo:rustc-link-search={}", lib.display());
 }
