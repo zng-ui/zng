fe70e8de65c7c97121fdf8b4876d2c8a573be09b
Samuel Guerra <sam.rodr.g@gmail.com>
2022-11-22 00:05:54 -0300

    Implemented `PropertyInputTypes` to assist in code generation. Will probably work for `easing`.

--- zero-ui-core/src/var/property_build_action.rs
+++ zero-ui-core/src/var/property_build_action.rs
@@ -1,4 +1,4 @@
-use std::{sync::Arc, time::Duration};
+use std::{any::TypeId, marker::PhantomData, sync::Arc, time::Duration};
 
 use crate::{
     units::*,
@@ -15,44 +15,104 @@ pub fn easing_property_build_action(
     todo!()
 }
 
-/// Represents a *type visitor* that visits the input types of a property.
-pub trait PropertyInputTypeVisitor {
-    /// Input is a [`BoxedVar<T>`].
-    fn visit_var<T: VarValue>(&mut self, index: usize) {
-        self.visit_input::<BoxedVar<T>>(index)
-    }
-
-    /// Visit any input type, other visit methods calls this method in their default implementation.
-    fn visit_input<I: Send + 'static>(&mut self, index: usize) {
-        let _ = index;
+/// Represents the strong types of each input of a property.
+///
+/// # Examples
+///
+/// The example uses [`property_input_types!`] to collect the types and compares it to a manually generated types. Note
+/// that the type is a tuple even if there is only one input.
+///
+/// ```
+/// # use zero_ui_core::{*, widget_instance::*, widget_builder::*};
+/// # use std::any::Any;
+/// #[property(CONTEXT)]
+/// pub fn foo(child: impl UiNode, bar: impl IntoVar<bool>) -> impl UiNode {
+/// #    child
+/// }
+///
+/// assert_eq!(
+///     property_input_types!(foo).type_id(),
+///     PropertyInputTypes::<(BoxedVar<bool>,)>::unit().type_id(),
+/// );
+/// ```
+///
+/// You can use the collected types in advanced code generation, such as attribute proc-macros targeting property assigns in widgets.
+/// The next example demonstrates a trait that uses auto-deref to convert a trait bound to a `bool`:
+///
+/// ```
+/// # use zero_ui_core::{*, widget_instance::*, widget_builder::*};
+/// #[property(CONTEXT)]
+/// pub fn foo(child: impl UiNode, bar: impl IntoVar<bool>) -> impl UiNode {
+/// #    child
+/// }
+///
+/// trait SingleBoolVar {
+///     fn is_single_bool_var(self) -> bool;
+/// }
+///
+/// // match
+/// impl<'a, V: Var<bool>> SingleBoolVar for &'a PropertyInputTypes<(V,)> {
+///     fn is_single_bool_var(self) -> bool {
+///         true
+///     }
+/// }
+///
+/// // fallback impl
+/// impl<T: Send + 'static> SingleBoolVar for PropertyInputTypes<T> {
+///     fn is_single_bool_var(self) -> bool {
+///         false
+///     }
+/// }
+///
+/// assert!((&property_input_types!(foo)).is_single_bool_var());
+/// ```
+/// 
+/// Learn more about how this trick works and limitations 
+/// [here](https://github.com/dtolnay/case-studies/blob/master/autoref-specialization/README.md).
+pub struct PropertyInputTypes<Tuple>(PhantomData<Tuple>);
+impl<Tuple> PropertyInputTypes<Tuple> {
+    /// Unit value.
+    pub fn unit() -> Self {
+        Self(PhantomData)
     }
 }
-
-pub fn __visit_types__(visitor: &mut impl PropertyInputTypeVisitor) {
-    visitor.visit_var::<bool>(0);
+impl<Tuple> Clone for PropertyInputTypes<Tuple> {
+    fn clone(&self) -> Self {
+        Self::unit()
+    }
 }
+impl<Tuple> Copy for PropertyInputTypes<Tuple> {}
 
-pub struct EasingPropertyBuildActionVisitor {
-    duration: Duration,
-    easing: Arc<dyn Fn(EasingTime) -> EasingStep + Send + Sync + 'static>,
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    actions: Vec<Box<dyn AnyPropertyBuildAction>>,
-    any_var: bool,
-}
-
-impl PropertyInputTypeVisitor for EasingPropertyBuildActionVisitor {
-    fn visit_var<T: VarValue>(&mut self, _: usize) {
-        let dur = self.duration;
-        let easing = self.easing.clone();
-        let action = PropertyBuildAction::<BoxedVar<T>>::new(move |var| {
-            let easing = easing.clone();
-            var.easing(dur, move |t| easing(t)).boxed()
-        });
-        self.actions.push(Box::new(action));
-        self.any_var = true;
+    #[test]
+    fn test() {
+        let input_types = PropertyInputTypes::<(BoxedVar<bool>,)>::unit();
+        assert!((&input_types).is_single_bool_var());
     }
 
-    fn visit_input<I: Send + 'static>(&mut self, index: usize) {
-        self.actions.push(Box::new(PropertyBuildAction::<I>::no_op()));
+    #[test]
+    fn test_generic() {
+        let input_types = PropertyInputTypes::<(BoxedVar<bool>,)>::unit();
+        assert!(generic_test(input_types)); // this fails
+    }
+
+    fn generic_test<Tuple>(input_types: PropertyInputTypes<Tuple>) -> bool {
+        (&input_types).is_single_bool_var()
+    }
+    trait SingleBoolVar {
+        fn is_single_bool_var(self) -> bool;
+    }
+    impl<'a, V: Var<bool>> SingleBoolVar for &'a PropertyInputTypes<(V,)> {
+        fn is_single_bool_var(self) -> bool {
+            true
+        }
+    }
+    impl<T> SingleBoolVar for PropertyInputTypes<T> {
+        fn is_single_bool_var(self) -> bool {
+            false
+        }
     }
 }
