85031ce0ba69c8f7bca02fbb0e67d08f957fc94c
Samuel Guerra <sam.rodr.g@gmail.com>
2022-12-07 23:43:30 -0300

    Improved inspector window.

--- zero-ui/src/widgets/window_wgt/commands.rs
+++ zero-ui/src/widgets/window_wgt/commands.rs
@@ -18,12 +18,21 @@ pub(super) fn inspect_node(
     child: impl crate::core::widget_instance::UiNode,
     can_inspect: impl crate::core::var::IntoVar<bool>,
 ) -> impl crate::core::widget_instance::UiNode {
-    use zero_ui_core::{units::Align, window::Windows};
+    use zero_ui_core::inspector::WidgetInfoInspectorExt;
 
-    use crate::core::handler::hn;
-    use crate::core::inspector::prompt::WriteTreeState;
+    use crate::core::{
+        handler::{clone_move, hn},
+        inspector::prompt::WriteTreeState,
+        text::{formatx, Text},
+        units::Align,
+        var::{var, var_from, Var},
+        widget_builder::property_id,
+        window::{WindowId, Windows},
+    };
 
-    let mut state = WriteTreeState::new();
+    let mut inspector_state = WriteTreeState::new();
+    let inspector_id = WindowId::new_unique();
+    let inspector_text = var(Text::empty());
 
     let can_inspect = can_inspect.into_var();
 
@@ -39,21 +48,33 @@ pub(super) fn inspect_node(
             args.propagation().stop();
 
             let mut buffer = vec![];
-            state.write_update(ctx.info_tree, &mut buffer);
+            inspector_state.write_update(ctx.info_tree, &mut buffer);
 
             let txt = String::from_utf8_lossy(&buffer).into_owned();
+            inspector_text.set(ctx, txt);
+
             let parent = ctx.path.window_id();
-            Windows::req(ctx.services).open(move |_| {
-                crate::widgets::window! {
-                    parent;
-                    title = "Inspector";
-                    child = crate::widgets::scroll! {
-                        child = crate::widgets::ansi_text! { txt; };
-                        child_align = Align::TOP_LEFT;
-                        padding = 5;
+            Windows::req(ctx).focus_or_open(
+                inspector_id,
+                clone_move!(inspector_text, |ctx| {
+                    let tree = Windows::req(ctx.services).widget_tree(parent);
+                    let title = if let Some(title) = tree.unwrap().root().inspect_property(property_id!(crate::widgets::window::title)) {
+                        title.downcast_var::<Text>(0).map(|t| formatx!("{t} - Inspector")).boxed()
+                    } else {
+                        var_from("Inspector").boxed()
+                    };
+
+                    crate::widgets::window! {
+                        parent;
+                        title;
+                        child = crate::widgets::scroll! {
+                            child = crate::widgets::ansi_text! { txt = inspector_text; };
+                            child_align = Align::TOP_LEFT;
+                            padding = 5;
+                        }
                     }
-                }
-            });
+                }),
+            );
 
             //task::spawn_wait(move || {
             //    use std::io::*;
