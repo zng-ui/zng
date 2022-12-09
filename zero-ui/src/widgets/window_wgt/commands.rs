178147022462ef6dbed08b4269c06588351a0400
Well <well-r@hotmail.com>
2022-12-09 00:58:32 -0300

    Changed the color_scheme and scroll mode of the inspector window.

--- zero-ui/src/widgets/window_wgt/commands.rs
+++ zero-ui/src/widgets/window_wgt/commands.rs
@@ -18,16 +18,18 @@ pub(super) fn inspect_node(
     child: impl crate::core::widget_instance::UiNode,
     can_inspect: impl crate::core::var::IntoVar<bool>,
 ) -> impl crate::core::widget_instance::UiNode {
-    use zero_ui_core::inspector::WidgetInfoInspectorExt;
-
-    use crate::core::{
-        handler::{clone_move, hn},
-        inspector::prompt::WriteTreeState,
-        text::{formatx, Text},
-        units::Align,
-        var::{var, var_from, Var},
-        widget_builder::property_id,
-        window::{WindowId, Windows},
+    use crate::{
+        core::{
+            color::ColorScheme,
+            handler::{clone_move, hn},
+            inspector::{prompt::WriteTreeState, WidgetInfoInspectorExt},
+            text::{formatx, Text},
+            units::Align,
+            var::{var, var_from, Var},
+            widget_builder::property_id,
+            window::{WindowId, Windows},
+        },
+        widgets::scroll::ScrollMode,
     };
 
     let mut inspector_state = WriteTreeState::new();
@@ -64,8 +66,10 @@ pub(super) fn inspect_node(
                     crate::widgets::window! {
                         parent;
                         title;
+                        color_scheme = ColorScheme::Dark;
                         child = crate::widgets::scroll! {
                             child = crate::widgets::ansi_text! { txt = inspector_text; };
+                            mode = ScrollMode::VERTICAL;
                             child_align = Align::TOP_LEFT;
                             padding = 5;
                         }
