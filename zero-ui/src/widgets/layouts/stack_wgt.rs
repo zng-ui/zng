52e22a67eefa3082b9fd992b5792f8d8017dc497
Samuel Guerra <sam.rodr.g@gmail.com>
2022-12-28 00:03:00 -0300

    Implemented stack measure.

--- zero-ui/src/widgets/layouts/stack_wgt.rs
+++ zero-ui/src/widgets/layouts/stack_wgt.rs
@@ -66,7 +66,8 @@ pub mod stack {
     #[var] spacing: impl Var<Length>,
     #[var] children_align: impl Var<Align>,
 })]
-impl UiNode for StackNode {
+impl StackNode {
+    #[UiNode]
     fn update(&mut self, ctx: &mut WidgetContext, updates: &mut WidgetUpdates) {
         let mut changed = false;
         self.children.update_all(ctx, updates, &mut changed);
@@ -76,76 +77,60 @@ impl UiNode for StackNode {
         }
     }
 
+    #[UiNode]
     fn measure(&self, ctx: &mut MeasureContext, wm: &mut WidgetMeasure) -> PxSize {
         let constrains = ctx.constrains();
         if let Some(known) = constrains.fill_or_exact() {
             return known;
         }
 
-        todo! {}
-    }
-
-    fn layout(&mut self, ctx: &mut LayoutContext, wl: &mut WidgetLayout) -> PxSize {
-        let constrains = ctx.constrains();
         let direction = self.direction.get();
-        let direction_vector = direction.vector(ctx.direction());
         let children_align = self.children_align.get();
         let child_align = direction.filter_align(children_align);
 
-        // !!: review this
-        let spacing = self.spacing.get();
-        let mut spacing = match (direction_vector.x == 0, direction_vector.y == 0) {
-            (false, false) => PxVector::new(spacing.layout(ctx.for_x(), |_| Px(0)), spacing.layout(ctx.for_y(), |_| Px(0))),
-            (true, false) => PxVector::new(Px(0), spacing.layout(ctx.for_y(), |_| Px(0))),
-            (false, true) => PxVector::new(spacing.layout(ctx.for_x(), |_| Px(0)), Px(0)),
-            (true, true) => PxVector::zero(),
-        };
-        if direction_vector.x < 0 {
-            spacing.x = -spacing.x;
-        }
-        if direction_vector.y < 0 {
-            spacing.y = -spacing.y;
-        }
+        let spacing = self.layout_spacing(ctx);
+        let max_size = self.child_max_size(ctx, child_align);
 
-        // need measure when children fill, but the panel does not.
-        let mut need_measure = false;
-        let mut max_size = PxSize::zero();
-        let mut measure_constrains = constrains;
-        match (constrains.x.fill_or_exact(), constrains.y.fill_or_exact()) {
-            (None, None) => {
-                need_measure = child_align.is_fill_x() || child_align.is_fill_y();
-                if !need_measure {
-                    max_size = constrains.max_size().unwrap_or_else(|| PxSize::new(Px::MAX, Px::MAX));
-                }
-            }
-            (None, Some(h)) => {
-                max_size.height = h;
-                need_measure = child_align.is_fill_x();
-                measure_constrains = constrains.with_fill_x(false);
-            }
-            (Some(w), None) => {
-                max_size.width = w;
-                need_measure = child_align.is_fill_y();
-                measure_constrains = constrains.with_fill_y(false);
-            }
-            (Some(w), Some(h)) => max_size = PxSize::new(w, h),
-        }
+        // layout children, size, raw position + spacing only.
+        let mut item_bounds = euclid::Box2D::zero();
+        ctx.with_constrains(
+            move |_| {
+                constrains
+                    .with_fill(child_align.is_fill_x(), child_align.is_fill_y())
+                    .with_max_size(max_size)
+            },
+            |ctx| {
+                let mut item_rect = PxRect::zero();
+                let mut child_spacing = PxVector::zero();
+                self.children.for_each(|_, c| {
+                    let size = c.measure(ctx, wm);
+                    let offset = direction.layout(ctx, item_rect, size) + child_spacing;
 
-        // find largest child, the others will fill to its size.
-        if need_measure {
-            ctx.as_measure().with_constrains(
-                move |_| measure_constrains,
-                |ctx| {
-                    self.children.for_each(|_, c| {
-                        let size = c.measure(ctx, &mut WidgetMeasure::new());
-                        max_size = max_size.max(size);
-                        true
-                    });
-                },
-            );
+                    item_rect.origin = offset.to_point();
+                    item_rect.size = size;
 
-            max_size = constrains.clamp_size(max_size);
-        }
+                    let item_box = item_rect.to_box2d();
+                    item_bounds.min = item_bounds.min.min(item_box.min);
+                    item_bounds.max = item_bounds.max.max(item_box.max);
+                    child_spacing = spacing;
+
+                    true
+                });
+            },
+        );
+
+        constrains.fill_size_or(item_bounds.size())
+    }
+
+    #[UiNode]
+    fn layout(&mut self, ctx: &mut LayoutContext, wl: &mut WidgetLayout) -> PxSize {
+        let constrains = ctx.constrains();
+        let direction = self.direction.get();
+        let children_align = self.children_align.get();
+        let child_align = direction.filter_align(children_align);
+
+        let spacing = self.layout_spacing(ctx);
+        let max_size = self.child_max_size(&mut ctx.as_measure(), child_align);
 
         // layout children, size, raw position + spacing only.
         let mut item_bounds = euclid::Box2D::zero();
@@ -180,7 +165,7 @@ impl UiNode for StackNode {
         // final position, align child inside item_bounds and item_bounds in the panel area.
         let child_align = child_align.xy(ctx.direction());
         let items_size = item_bounds.size();
-        let panel_size = constrains.fill_size_or(item_bounds.size());
+        let panel_size = constrains.fill_size_or(items_size);
         let children_offset = -item_bounds.min.to_vector() + (panel_size - items_size).to_vector() * children_align.xy(ctx.direction());
 
         // !!: underline align?
@@ -193,5 +178,73 @@ impl UiNode for StackNode {
         });
 
         panel_size
+    }
+
+    /// Spacing to add on each axis.
+    fn layout_spacing(&self, ctx: &LayoutMetrics) -> PxVector {
+        let direction_vector = self.direction.get().vector(ctx.direction());
+
+        let spacing = self.spacing.get();
+        let mut spacing = match (direction_vector.x == 0, direction_vector.y == 0) {
+            (false, false) => PxVector::new(spacing.layout(ctx.for_x(), |_| Px(0)), spacing.layout(ctx.for_y(), |_| Px(0))),
+            (true, false) => PxVector::new(Px(0), spacing.layout(ctx.for_y(), |_| Px(0))),
+            (false, true) => PxVector::new(spacing.layout(ctx.for_x(), |_| Px(0)), Px(0)),
+            (true, true) => PxVector::zero(),
+        };
+        if direction_vector.x < 0 {
+            spacing.x = -spacing.x;
+        }
+        if direction_vector.y < 0 {
+            spacing.y = -spacing.y;
+        }
+
+        spacing
+    }
+
+    /// Max size to layout each child with.
+    fn child_max_size(&self, ctx: &mut MeasureContext, child_align: Align) -> PxSize {
+        let constrains = ctx.constrains();
+
+        // need measure when children fill, but the panel does not.
+        let mut need_measure = false;
+        let mut max_size = PxSize::zero();
+        let mut measure_constrains = constrains;
+        match (constrains.x.fill_or_exact(), constrains.y.fill_or_exact()) {
+            (None, None) => {
+                need_measure = child_align.is_fill_x() || child_align.is_fill_y();
+                if !need_measure {
+                    max_size = constrains.max_size().unwrap_or_else(|| PxSize::new(Px::MAX, Px::MAX));
+                }
+            }
+            (None, Some(h)) => {
+                max_size.height = h;
+                need_measure = child_align.is_fill_x();
+                measure_constrains = constrains.with_fill_x(false);
+            }
+            (Some(w), None) => {
+                max_size.width = w;
+                need_measure = child_align.is_fill_y();
+                measure_constrains = constrains.with_fill_y(false);
+            }
+            (Some(w), Some(h)) => max_size = PxSize::new(w, h),
+        }
+
+        // find largest child, the others will fill to its size.
+        if need_measure {
+            ctx.with_constrains(
+                move |_| measure_constrains,
+                |ctx| {
+                    self.children.for_each(|_, c| {
+                        let size = c.measure(ctx, &mut WidgetMeasure::new());
+                        max_size = max_size.max(size);
+                        true
+                    });
+                },
+            );
+
+            max_size = constrains.clamp_size(max_size);
+        }
+
+        max_size
     }
 }
