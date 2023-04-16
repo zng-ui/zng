55eaaca76c86e925c0b3f0a47d2870467129fb1c
Samuel Guerra <sam.rodr.g@gmail.com>
2023-04-16 12:21:55 -0300

    fmt

--- zero-ui/src/widgets/window/nodes.rs
+++ zero-ui/src/widgets/window/nodes.rs
@@ -567,10 +567,10 @@ pub fn on_layer_remove_requested(child: impl UiNode, handler: impl WidgetHandler
                 let args = LayerRemoveRequestedArgs::now();
                 self.handler.event(&args);
                 match args.propagation().is_stopped() {
-                    true => {                        
+                    true => {
                         WIDGET.set_state(&LAYER_REMOVE_ID, LayerRemove::Request);
                     }
-                    false => {                        
+                    false => {
                         WIDGET.set_state(&LAYER_REMOVE_ID, LayerRemove::Allowed);
                         list.remove(WIDGET.id());
                     }
