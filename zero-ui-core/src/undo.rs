6d2a4fa52733bf220c480feb39018b626f77fc2f
Samuel Guerra <sam.rodr.g@gmail.com>
2023-07-01 22:12:14 -0300

    More undo work.

--- zero-ui-core/src/undo.rs
+++ zero-ui-core/src/undo.rs
@@ -1,14 +1,22 @@
 //! Undo-redo app extension, service and commands.
 //!
 
-use std::{any::Any, fmt, time::Duration};
+use std::{any::Any, fmt, sync::atomic::AtomicBool, time::Duration};
 
 use atomic::{Atomic, Ordering};
 use parking_lot::Mutex;
 
 use crate::{
-    app::AppExtension, app_local, command, context::WIDGET, context_local, event::CommandNameExt, gesture::CommandShortcutExt, shortcut,
-    units::*, var::*, widget_instance::WidgetId,
+    app::AppExtension,
+    app_local, command,
+    context::WIDGET,
+    context_local,
+    event::{AnyEventArgs, CommandNameExt},
+    gesture::CommandShortcutExt,
+    shortcut,
+    units::*,
+    var::*,
+    widget_instance::WidgetId,
 };
 
 /// Undo-redo app extension.
@@ -27,7 +35,18 @@ use crate::{
 #[derive(Default)]
 pub struct UndoManager {}
 
-impl AppExtension for UndoManager {}
+impl AppExtension for UndoManager {
+    fn event(&mut self, update: &mut crate::event::EventUpdate) {
+        // app scope handler
+        if let Some(args) = UNDO_CMD.on_unhandled(update) {
+            args.propagation().stop();
+            UNDO.undo(args.param::<u32>().copied().unwrap_or(1));
+        } else if let Some(args) = UNDO_CMD.on_unhandled(update) {
+            args.propagation().stop();
+            UNDO.redo(args.param::<u32>().copied().unwrap_or(1));
+        }
+    }
+}
 
 /// Undo-redo service.
 pub struct UNDO;
@@ -84,39 +103,45 @@ pub trait RedoAction: UndoRedoItem {
 
 command! {
     /// Represents the **undo** action.
+    ///
+    /// # Param
+    ///
+    /// If the command parameter is a `u32` it is the count of undo actions to run, otherwise runs `1` action.
     pub static UNDO_CMD = {
         name: "Undo",
         shortcut: [shortcut!(CTRL+Z)],
     };
 
     /// Represents the clipboard **redo** action.
+    ///
+    /// # Param
+    ///
+    /// If the command parameter is a `u32` it is the count of redo actions to run, otherwise runs `1` action.
     pub static REDO_CMD = {
         name: "Redo",
         shortcut: [shortcut!(CTRL+Y)],
     };
 }
-
+#[derive(Default)]
 struct UndoScope {
     id: Atomic<Option<WidgetId>>,
     undo: Mutex<Vec<Box<dyn UndoAction>>>,
     redo: Mutex<Vec<Box<dyn RedoAction>>>,
-}
-impl Default for UndoScope {
-    fn default() -> Self {
-        Self {
-            id: Atomic::new(WIDGET.try_id()),
-            undo: Mutex::new(vec![]),
-            redo: Mutex::new(vec![]),
-        }
-    }
+    enabled: AtomicBool,
 }
 impl UndoScope {
     fn register(&self, action: Box<dyn UndoAction>) {
+        if !self.enabled.load(Ordering::Relaxed) {
+            return;
+        }
         self.undo.lock().push(action);
         self.redo.lock().clear();
     }
 
     fn undo(&self, mut count: u32) {
+        if !self.enabled.load(Ordering::Relaxed) {
+            return;
+        }
         while count > 0 {
             count -= 1;
 
@@ -128,6 +153,9 @@ impl UndoScope {
     }
 
     fn redo(&self, mut count: u32) {
+        if !self.enabled.load(Ordering::Relaxed) {
+            return;
+        }
         while count > 0 {
             count -= 1;
 
@@ -135,6 +163,14 @@ impl UndoScope {
                 let undo = redo.redo();
                 self.undo.lock().push(undo);
             }
+        }
+    }
+
+    fn set_enabled(&self, enabled: bool) {
+        self.enabled.store(enabled, Ordering::Relaxed);
+        if !enabled {
+            self.undo.lock().clear();
+            self.redo.lock().clear();
         }
     }
 
@@ -163,3 +199,72 @@ context_local! {
 app_local! {
     static UNDO_SV: UndoService = UndoService::default();
 }
+
+mod properties {
+    use std::sync::Arc;
+
+    use super::*;
+    use crate::{event::CommandHandle, widget_instance::*, *};
+
+    /// Defines an undo/redo scope in the widget.
+    ///
+    /// If `enabled` is `false` undo/redo is disabled for the widget and descendants, if it is
+    /// `true` all undo/redo actions
+    #[property(WIDGET)]
+    pub fn undo_scope(child: impl UiNode, enabled: impl IntoVar<bool>) -> impl UiNode {
+        let mut scope = None;
+        let mut undo_cmd = CommandHandle::dummy();
+        let mut redo_cmd = CommandHandle::dummy();
+        let enabled = enabled.into_var();
+        match_node(child, move |c, op| {
+            match op {
+                UiNodeOp::Init => {
+                    let id = WIDGET.id();
+                    let s = UndoScope {
+                        id: Atomic::new(Some(id)),
+                        enabled: AtomicBool::new(enabled.get()),
+                        ..Default::default()
+                    };
+                    scope = Some(Arc::new(s));
+
+                    undo_cmd = UNDO_CMD.scoped(id).subscribe(false);
+                    redo_cmd = REDO_CMD.scoped(id).subscribe(false);
+
+                    WIDGET.sub_var(&enabled);
+                }
+                UiNodeOp::Deinit => {
+                    UNDO_SCOPE_CTX.with_context(&mut scope, || c.deinit());
+                    scope = None;
+                    undo_cmd = CommandHandle::dummy();
+                    redo_cmd = CommandHandle::dummy();
+                    return;
+                }
+                UiNodeOp::Event { update } => {
+                    let id = WIDGET.id();
+                    if let Some(args) = UNDO_CMD.scoped(id).on_unhandled(update) {
+                        args.propagation().stop();
+                        let scope = scope.as_ref().unwrap();
+                        scope.undo(args.param::<u32>().copied().unwrap_or(1));
+                    } else if let Some(args) = REDO_CMD.scoped(id).on_unhandled(update) {
+                        args.propagation().stop();
+                        let scope = scope.as_ref().unwrap();
+                        scope.redo(args.param::<u32>().copied().unwrap_or(1));
+                    }
+                }
+                UiNodeOp::Update { .. } => {
+                    if let Some(enabled) = enabled.get_new() {
+                        scope.as_ref().unwrap().set_enabled(enabled);
+                    }
+                }
+                _ => {}
+            }
+
+            UNDO_SCOPE_CTX.with_context(&mut scope, || c.op(op));
+
+            let scope = scope.as_ref().unwrap();
+            undo_cmd.set_enabled(!scope.undo.lock().is_empty());
+            redo_cmd.set_enabled(!scope.redo.lock().is_empty());
+        })
+    }
+}
+pub use properties::undo_scope;
