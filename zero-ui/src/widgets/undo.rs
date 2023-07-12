cfb4d08f2d4faf0868e3319695655f8d663dd8ac
Samuel Guerra <sam.rodr.g@gmail.com>
2023-07-12 12:54:39 -0300

    More UndoHistory! work.

--- zero-ui/src/widgets/undo.rs
+++ zero-ui/src/widgets/undo.rs
@@ -8,6 +8,7 @@ use crate::prelude::new_widget::*;
 
 use crate::core::gesture::ClickArgs;
 use crate::core::undo::{UndoInfo, UndoOp, REDO_CMD, UNDO, UNDO_CMD};
+use crate::widgets::button;
 use crate::widgets::{
     layouts::{stack::StackDirection, Stack},
     view, Button, Text,
@@ -76,7 +77,11 @@ context_var! {
 
 /// Widget function that converts [`UndoEntryArgs`] to widgets.
 ///
+/// Try [`extend_undo_button_style`] for making only visual changes.
+///
 /// Sets the [`UNDO_ENTRY_FN_VAR`].
+///
+/// [`extend_undo_button_style`]: fn@extend_undo_button_style
 #[property(CONTEXT+1, default(UNDO_ENTRY_FN_VAR), widget_impl(UndoHistory))]
 pub fn undo_entry_fn(child: impl UiNode, wgt_fn: impl IntoVar<WidgetFn<UndoEntryArgs>>) -> impl UiNode {
     with_context_var(child, UNDO_ENTRY_FN_VAR, wgt_fn)
@@ -100,7 +105,7 @@ pub fn undo_panel_fn(child: impl UiNode, wgt_fn: impl IntoVar<WidgetFn<UndoPanel
 
 /// Default [`UNDO_ENTRY_FN_VAR`].
 ///
-/// Returns a `Button!` with the [`UndoRedoButtonStyle!`] and the entry displayed in a `Text!` child.
+/// Returns a `Button!` with the [`UNDO_BUTTON_STYLE_VAR`] and the entry displayed in a `Text!` child.
 /// The button notifies [`UNDO_CMD`] or [`REDO_CMD`] with the entry timestamp, the command is scoped on the
 /// undo parent of the caller not of the button.
 ///
@@ -116,7 +121,8 @@ pub fn default_undo_entry_fn(args: UndoEntryArgs) -> impl UiNode {
     let ts = args.timestamp;
     Button! {
         child = Text!(args.info.description());
-        style_fn = UndoRedoButtonStyle!();
+        undo_entry = args;
+        style_fn = UNDO_BUTTON_STYLE_VAR;
         on_click = hn!(|args: &ClickArgs| {
             args.propagation().stop();
             cmd.notify_param(ts);
@@ -145,6 +151,7 @@ pub fn default_undo_stack_fn(args: UndoStackArgs) -> impl UiNode {
         .collect::<UiNodeVec>();
 
     Stack! {
+        undo_stack = args.op;
         direction = StackDirection::top_to_bottom();
         children;
     }
@@ -233,14 +240,109 @@ pub struct UndoPanelArgs {}
 
 /// Menu style button for an entry in a undo/redo stack.
 #[widget($crate::widgets::undo::UndoRedoButtonStyle)]
-pub struct UndoRedoButtonStyle(crate::widgets::button::DefaultStyle);
+pub struct UndoRedoButtonStyle(Style);
 impl UndoRedoButtonStyle {
     fn widget_intrinsic(&mut self) {
         widget_set! {
             self;
-            corner_radius = unset!;
-            border = unset!;
-            padding = (4, 6);
+            padding = 4;
+            child_align = Align::START;
+
+            #[easing(150.ms())]
+            background_color = color_scheme_pair(button::BASE_COLORS_VAR);
+
+            when *#is_cap_hovered_timestamp {
+                #[easing(0.ms())]
+                background_color = button::color_scheme_hovered(button::BASE_COLORS_VAR);
+            }
+
+            when *#is_pressed {
+                #[easing(0.ms())]
+                background_color = button::color_scheme_pressed(button::BASE_COLORS_VAR);
+            }
         }
     }
+}
+
+context_var! {
+    /// Variable set by the parent undo/redo stack widget, can be used to highlight items
+    /// that will be included in the undo/redo operation.
+    static HOVERED_TIMESTAMP_VAR: Option<Instant> = None;
+
+    /// Variable set in each undo/redo entry widget.
+    pub static UNDO_ENTRY_VAR: Option<UndoEntryArgs> = None;
+
+    /// Variable set in each undo/redo stack widget.
+    pub static UNDO_STACK_VAR: Option<UndoOp> = None;
+
+    /// Style for the default undo/redo entry [`Button!`].
+    ///
+    /// Is [`UndoRedoButtonStyle!`] by default.
+    ///
+    /// [`UndoRedoButtonStyle!`]: struct@UndoRedoButtonStyle
+    /// [`Button!`]: struct@Button
+    pub static UNDO_BUTTON_STYLE_VAR: StyleFn = style_fn!(|_| UndoRedoButtonStyle!());
+}
+
+/// Sets the undo/redo entry button style in a context, the parent style is fully replaced.
+#[property(CONTEXT, default(UNDO_BUTTON_STYLE_VAR))]
+pub fn replace_undo_button_style(child: impl UiNode, style: impl IntoVar<StyleFn>) -> impl UiNode {
+    with_context_var(child, UNDO_BUTTON_STYLE_VAR, style)
+}
+
+/// Extends the undo/redo entry button style in a context, the parent style is used, properties of the same name set in
+/// `style` override the parent style.
+#[property(CONTEXT, default(StyleFn::nil()))]
+pub fn extend_undo_button_style(child: impl UiNode, style: impl IntoVar<StyleFn>) -> impl UiNode {
+    style::with_style_extension(child, UNDO_BUTTON_STYLE_VAR, style)
+}
+
+/// Sets the undo/redo entry widget context.
+///
+/// In the widget style the [`UNDO_ENTRY_VAR`] can be used to access the [`UndoEntryArgs`].
+#[property(CONTEXT-1)]
+pub fn undo_entry(child: impl UiNode, entry: impl IntoValue<UndoEntryArgs>) -> impl UiNode {
+    let entry = entry.into();
+
+    // set the hovered timestamp
+    let timestamp = entry.timestamp;
+    let is_hovered = var(false);
+    let child = is_cap_hovered(child, is_hovered.clone());
+    let child = match_node(child, move |_, op| {
+        if let UiNodeOp::Init = op {
+            is_hovered
+                .bind_map(&HOVERED_TIMESTAMP_VAR, move |&ih| if ih { Some(timestamp) } else { None })
+                .perm();
+        }
+    });
+
+    with_context_var(child, UNDO_ENTRY_VAR, Some(entry))
+}
+
+/// Setups the context in an undo/redo stack widget.
+///
+/// If this is not set in the stack widget the entry widgets may not work properly.
+#[property(CONTEXT-1)]
+pub fn undo_stack(child: impl UiNode, op: impl IntoValue<UndoOp>) -> impl UiNode {
+    let child = with_context_var(child, HOVERED_TIMESTAMP_VAR, var(None));
+    with_context_var(child, UNDO_STACK_VAR, Some(op.into()))
+}
+
+/// State is true when the widget is an [`undo_entry`] and it is hovered, has captured the mouse
+/// or a sibling with higher timestamp is hovered/has cap.
+///
+/// [`undo_entry`]: fn@undo_entry
+#[property(CONTEXT)]
+pub fn is_cap_hovered_timestamp(child: impl UiNode, state: impl IntoVar<bool>) -> impl UiNode {
+    // check the hovered timestamp
+    bind_is_state(
+        child,
+        merge_var!(HOVERED_TIMESTAMP_VAR, UNDO_ENTRY_VAR, |&ts, entry| {
+            match (ts, entry) {
+                (Some(ts), Some(entry)) => entry.timestamp <= ts,
+                _ => false,
+            }
+        }),
+        state,
+    )
 }
