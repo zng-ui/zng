ff6a904fc548a7aa70baa78d81610b52cfa2a8f1
Samuel Guerra <sam.rodr.g@gmail.com>
2023-09-26 12:14:39 -0300

    Updated icu.

--- zero-ui-core/src/text/emoji_util.rs
+++ zero-ui-core/src/text/emoji_util.rs
@@ -1,12 +1,6 @@
 /*
 Loaded data is !Send+!Sync so we probably don't need to cache it.
-
-The "icu_testdata" includes the stuff we need, plus a lot of useless data, there is a complicated way to
-optmize this, but they are about to release embedded data, so we wait.
-
-see: see https://github.com/unicode-org/icu4x/issues/3529
-
- */
+*/
 
 use std::{fmt, mem::size_of};
 
@@ -17,29 +11,19 @@ use zero_ui_view_api::webrender_api::GlyphIndex;
 use crate::{color::Rgba, impl_from_and_into_var};
 
 pub(super) fn maybe_emoji(c: char) -> bool {
-    sets::load_emoji(&icu_testdata::unstable()).unwrap().as_borrowed().contains(c)
+    sets::emoji().contains(c)
 }
 
 pub(super) fn definitely_emoji(c: char) -> bool {
-    sets::load_emoji_presentation(&icu_testdata::unstable())
-        .unwrap()
-        .as_borrowed()
-        .contains(c)
-        || is_modifier(c)
+    sets::emoji_presentation().contains(c) || is_modifier(c)
 }
 
 pub(super) fn is_modifier(c: char) -> bool {
-    sets::load_emoji_modifier(&icu_testdata::unstable())
-        .unwrap()
-        .as_borrowed()
-        .contains(c)
+    sets::emoji_modifier().contains(c)
 }
 
 pub(super) fn is_component(c: char) -> bool {
-    sets::load_emoji_component(&icu_testdata::unstable())
-        .unwrap()
-        .as_borrowed()
-        .contains(c)
+    sets::emoji_component().contains(c)
 }
 
 /*
