e2a6eda7beef8d1e1ef8dce4a47cbcb2e51b173a
Samuel Guerra <sam.rodr.g@gmail.com>
2023-10-25 18:27:41 -0300

    Better Label behaviour.

--- zero-ui/src/widgets/label.rs
+++ zero-ui/src/widgets/label.rs
@@ -1,12 +1,18 @@
 //! Label text.
 
-use crate::core::focus::{FOCUS, FOCUS_CHANGED_EVENT};
+use crate::core::{
+    access::ACCESS_CLICK_EVENT,
+    focus::{FocusInfoBuilder, FOCUS},
+    mouse::MOUSE_INPUT_EVENT,
+    touch::TOUCH_INPUT_EVENT,
+};
 
 use crate::prelude::new_widget::*;
 
 /// Styleable and focusable read-only text widget.
 ///
-/// Optionally can be the label of a [`target`] widget, automatically transferring focus to it.
+/// Optionally can be the label of a [`target`] widget, in this case the label is not focusable, it transfers focus
+/// to the target.
 #[widget($crate::widgets::Label)]
 pub struct Label(FocusableMix<StyleMix<EnabledMix<text::Text>>>);
 impl Label {
@@ -33,8 +39,10 @@ pub struct DefaultStyle(Style);
 
 /// Defines the widget the label is for.
 ///
-/// When the label is focused the widget or the first focusable child of the widget is focused.
+/// When the label is pressed the widget or the first focusable child of the widget is focused.
 /// Access metadata is also set so the target widget is marked as *labelled-by* this widget in the view-process.
+///
+/// If this is set focusable is disabled on the label widget.
 #[property(CONTEXT, widget_impl(Label))]
 pub fn target(child: impl UiNode, target: impl IntoVar<WidgetId>) -> impl UiNode {
     let target = target.into_var();
@@ -42,9 +50,17 @@ pub fn target(child: impl UiNode, target: impl IntoVar<WidgetId>) -> impl UiNode
 
     match_node(child, move |c, op| match op {
         UiNodeOp::Init => {
-            WIDGET.sub_var(&target).sub_event(&FOCUS_CHANGED_EVENT);
+            WIDGET
+                .sub_var(&target)
+                .sub_event(&MOUSE_INPUT_EVENT)
+                .sub_event(&TOUCH_INPUT_EVENT)
+                .sub_event(&ACCESS_CLICK_EVENT);
         }
         UiNodeOp::Info { info } => {
+            c.info(info);
+
+            FocusInfoBuilder::new(info).focusable(false);
+
             if let Some(mut a) = info.access() {
                 let target = target.get();
                 prev_target = Some(target);
@@ -53,10 +69,18 @@ pub fn target(child: impl UiNode, target: impl IntoVar<WidgetId>) -> impl UiNode
         }
         UiNodeOp::Event { update } => {
             c.event(update);
-            if let Some(args) = FOCUS_CHANGED_EVENT.on_unhandled(update) {
-                if args.is_focus_enter(WIDGET.id()) {
-                    args.propagation().stop();
-                    FOCUS.focus_widget_or_enter(target.get(), true, args.highlight);
+
+            if let Some(args) = MOUSE_INPUT_EVENT.on(update) {
+                if args.is_mouse_down() {
+                    FOCUS.focus_widget_or_enter(target.get(), true, false);
+                }
+            } else if let Some(args) = TOUCH_INPUT_EVENT.on(update) {
+                if args.is_touch_start() {
+                    FOCUS.focus_widget_or_enter(target.get(), true, false);
+                }
+            } else if let Some(args) = ACCESS_CLICK_EVENT.on(update) {
+                if args.is_primary {
+                    FOCUS.focus_widget_or_enter(target.get(), true, false);
                 }
             }
         }
