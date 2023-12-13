df178e8b697789301c3f13e7f60b25698bd7fafb
Samuel Guerra <sam.rodr.g@gmail.com>
2023-12-13 13:49:43 -0300

    Fixed inspector node collapsing window content.

--- zero-ui-wgt-inspector/src/lib.rs
+++ zero-ui-wgt-inspector/src/lib.rs
@@ -24,9 +24,23 @@ command! {
 /// Setup the inspector for the window.
 #[property(WIDGET)]
 pub fn inspector(child: impl UiNode, mut inspector: impl UiNode) -> impl UiNode {
-    match_node(child, move |c, mut op| {
-        c.op(op.reborrow());
-        inspector.op(op);
+    match_node(child, move |c, op| match op {
+        UiNodeOp::Measure { wm, desired_size } => {
+            *desired_size = c.measure(wm);
+            LAYOUT.with_constraints(PxConstraints2d::new_exact_size(*desired_size), || {
+                let _ = inspector.measure(wm);
+            });
+        }
+        UiNodeOp::Layout { wl, final_size } => {
+            *final_size = c.layout(wl);
+            LAYOUT.with_constraints(PxConstraints2d::new_exact_size(*final_size), || {
+                let _ = inspector.layout(wl);
+            });
+        }
+        mut op => {
+            c.op(op.reborrow());
+            inspector.op(op);
+        }
     })
 }
 
