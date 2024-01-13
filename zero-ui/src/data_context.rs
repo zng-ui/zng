cd57a6cbe1513edd47d5ac312a3305fdace29344
Samuel Guerra <sam.rodr.g@gmail.com>
2024-01-13 01:20:29 -0300

    Finished data context docs.

--- zero-ui/src/data_context.rs
+++ zero-ui/src/data_context.rs
@@ -4,17 +4,20 @@
 //! [`DATA`] service can then be used on the widget or descendant widgets to retrieve the data and to set validation annotations
 //! about the data.
 //!
-//! The example below TODO!
+//! The example below demonstrates a simple [MVVM] implementation using the data context to share the view-model instance
+//! with all widgets in the view. The example also uses the data annotations API to show data validation errors.
+//!
+//! [MVVM]: https://en.wikipedia.org/wiki/Model%E2%80%93view%E2%80%93viewmodel
 //!
 //! ```
 //! mod view {
 //!     use crate::view_model::*;
-//!     use zero_ui::{data_context, prelude::*, text_input, window::WindowRoot};
+//!     use zero_ui::{data_context, prelude::*, window::WindowRoot};
 //!
 //!     pub fn window() -> WindowRoot {
 //!         Window! {
-//!             // set data context for entire window.
-//!             data = ViewModel::default();
+//!             // set data context for entire window, using `var` to be read-write.
+//!             data = var(ViewModel::new(crate::model::connect()));
 //!
 //!             // bind title from data context.
 //!             title = DATA.req::<ViewModel>().map(|vm| vm.title());
@@ -23,66 +26,87 @@
 //!     }
 //!
 //!     fn content() -> impl UiNode {
+//!         // `req` panics if context is not set to the same type.
+//!         let vm = DATA.req::<ViewModel>();
 //!         Container! {
 //!             child = TextInput! {
-//!                 // bind data context, `req` panics if context is not set to the same type.
-//!                 txt = DATA.req::<ViewModel>().map_ref_bidi(|vm| vm.typing(), |vm| vm.typing_mut());
+//!                 txt = vm.map_ref_bidi(|vm| vm.new_item(), |vm| vm.new_item_mut());
+//!
+//!                 // FieldStyle shows data errors.
+//!                 style_fn = style_fn!(|_| zero_ui::text_input::FieldStyle!());
+//!                 data_context::data_error = vm.map_ref(|vm| vm.new_item_error());
 //!             };
 //!             child_bottom = Button! {
 //!                 child = Text!("Submit");
-//!                 widget::enabled = DATA.req::<ViewModel>().map(|vm| !vm.typing().is_empty());
-//!                 on_click = hn!(|_| {
-//!                     // use data context directly.
-//!                     DATA.req::<ViewModel>().modify(|vm| vm.to_mut().submit()).unwrap()
-//!                 });
+//!                 widget::enabled = vm.map(|vm| !vm.new_item().is_empty());
+//!                 on_click = hn!(|_| vm.modify(|vm| vm.to_mut().submit()).unwrap());
 //!             }, 5;
-//!
-//!             // set data error for all widgets in this container.
-//!             data_context::data_error = DATA.req::<ViewModel>().map_ref(|vm| vm.last_error());
-//!             // data_context::FieldStyle displays data errors.
-//!             text_input::replace_style = style_fn!(|_| text_input::FieldStyle!());
+//!             padding = 5;
 //!         }
 //!     }
 //! }
 //!
 //! mod view_model {
+//!     use crate::model::Model;
 //!     use zero_ui::text::*;
 //!
-//!     #[derive(Clone, Debug, PartialEq, Default)]
+//!     #[derive(Clone, Debug, PartialEq)]
 //!     pub struct ViewModel {
-//!         items: Vec<u32>,
-//!         typing: Txt,
-//!         last_error: Txt,
+//!         model: Model,
+//!         new_item: Txt,
+//!         new_item_error: Txt,
 //!     }
 //!     impl ViewModel {
+//!         pub fn new(model: Model) -> Self {
+//!             Self {
+//!                 model,
+//!                 new_item: Txt::from(""),
+//!                 new_item_error: Txt::from(""),
+//!             }
+//!         }
+//!
 //!         pub fn title(&self) -> Txt {
-//!             formatx!("App - {} items", self.items.len())
+//!             formatx!("App - {} entries", self.model.read().len())
 //!         }
 //!
-//!         pub fn typing(&self) -> &Txt {
-//!             &self.typing
+//!         pub fn new_item(&self) -> &Txt {
+//!             &self.new_item
 //!         }
-//!         pub fn typing_mut(&mut self) -> &mut Txt {
-//!             &mut self.typing
+//!         pub fn new_item_mut(&mut self) -> &mut Txt {
+//!             self.new_item_error = Txt::from("");
+//!             &mut self.new_item
 //!         }
 //!
-//!         pub fn last_error(&self) -> &Txt {
-//!             &self.last_error
+//!         pub fn new_item_error(&self) -> &Txt {
+//!             &self.new_item_error
 //!         }
 //!
 //!         pub fn submit(&mut self) {
-//!             match self.typing.parse::<u32>() {
+//!             match self.new_item.parse::<u32>() {
 //!                 Ok(item) => {
-//!                     self.items.push(item);
-//!                     self.last_error = Txt::from("");
-//!                     self.typing = Txt::from("");
+//!                     self.model.write().push(item);
+//!                     self.new_item_mut().clear();
 //!                 }
-//!                 Err(e) => self.last_error = e.to_txt(),
+//!                 Err(e) => self.new_item_error = e.to_txt(),
 //!             }
 //!         }
 //!     }
 //! }
+//!
+//! mod model {
+//!     use zero_ui::{task::parking_lot::RwLock, var::ArcEq};
+//!
+//!     pub type Model = ArcEq<RwLock<Vec<u32>>>;
+//!
+//!     pub fn connect() -> ArcEq<RwLock<Vec<u32>>> {
+//!         ArcEq::new(RwLock::new(vec![]))
+//!     }
+//! }
 //! ```
+//!
+//! Note that vars clone the value when modify is requested, so the view-model should probably use shared
+//! references to the model data, overall this cloning has no noticeable impact as it only happens once
+//! per user interaction in the worst case.
 //!
 //! [`data`]: fn@data
 //! [`VarValue`]: crate::var::VarValue
