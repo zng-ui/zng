31df4f685cd0ee482f3b52ecb3d7748662d0810b
Samuel Guerra <sam.rodr.g@gmail.com>
2024-01-16 16:51:30 -0300

    Fixed doctest build.

--- zero-ui/src/l10n.rs
+++ zero-ui/src/l10n.rs
@@ -5,6 +5,6 @@
 //! See [`zero_ui_ext_l10n`] for the full localization API.
 
 pub use zero_ui_ext_l10n::{
-    IntoL10nVar, L10nArgument, L10nDir, L10nMessageBuilder, L10nSource, Lang, LangMap, LangResource, LangResourceStatus, LangResources,
-    Langs, NilL10nSource, SwapL10nSource, L10N, LANG_VAR,
+    lang, IntoL10nVar, L10nArgument, L10nDir, L10nMessageBuilder, L10nSource, Lang, LangMap, LangResource, LangResourceStatus,
+    LangResources, Langs, NilL10nSource, SwapL10nSource, L10N, LANG_VAR,
 };
