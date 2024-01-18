7ccd907a9d99a7feeb246c79006e65f19765ebf7
Samuel Guerra <sam.rodr.g@gmail.com>
2024-01-18 14:29:30 -0300

    Added custom icon font doc example.

--- zero-ui/src/icon.rs
+++ zero-ui/src/icon.rs
@@ -24,7 +24,30 @@
 //! You can implement your own icon sets by providing [`GlyphIcon`] instances or a type that converts to `GlyphIcon`, the
 //! [`MaterialIcon`] type is an example of this. Glyph icons define a font name and a [`GlyphSource`] that can be a `char`
 //! or a ligature text.
+//! 
+//! ```
+//! # fn main() { }
+//! use zero_ui::{prelude::*, icon};
+//! # fn demo() {
+//! # let _ = APP.defaults();
+//! 
+//! let font = CustomFont::from_file(
+//!     "Font Awesome 6 Free-Regular",
+//!     r#"Font Awesome 6 Free-Regular-400.otf"#,
+//!     0,
+//! );
+//! FONTS.register(font).wait_into_rsp().await.unwrap();
+//! 
+//! # let _ =
+//! icon::Icon! {
+//!     ico = icon::GlyphIcon::new("Font Awesome 6 Free-Regular", "address-book").with_ligatures();
+//!     ico_size = 80;
+//! }
+//! # ;
+//! # }
+//! ```
 //!
+//! The example above loads an icon font and display one of the icons selected using a ligature that matches `"address-book"`.
 //!
 //! # Full API
 //!
