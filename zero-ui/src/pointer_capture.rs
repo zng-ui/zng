c7027f8717e8ee92d90b634243ac7812eaaef5c8
Samuel Guerra <sam.rodr.g@gmail.com>
2024-01-22 19:01:33 -0300

    Added pointer_capture docs.

--- zero-ui/src/pointer_capture.rs
+++ zero-ui/src/pointer_capture.rs
@@ -1,5 +1,41 @@
 //! Pointer capture service, properties, events and types.
 //!
+//! Pointer events target to the topmost widget under the pointer by default, the [`POINTER_CAPTURE`] service
+//! can be used to *capture* the pointer for an widget so that it remains the target for pointer events. The
+//! [`capture_pointer`](fn@capture_pointer) property can be used to automatically capture the pointer when pressed.
+//!
+//! ```
+//! use zero_ui::prelude::*;
+//! # let _scope = APP.defaults();
+//!
+//! # let _ =
+//! Wgt! {
+//!     zero_ui::pointer_capture::capture_pointer = true;
+//!
+//!     zero_ui::pointer_capture::on_got_pointer_capture = hn!(|_| {
+//!         println!("got capture");
+//!     });
+//!     zero_ui::pointer_capture::on_lost_pointer_capture = hn!(|_| {
+//!         println!("lost capture");
+//!     });
+//!
+//!     when *#gesture::is_cap_hovered {
+//!         widget::background_color = colors::GREEN;
+//!     }
+//!     widget::background_color = colors::RED;
+//!     layout::size = 80;
+//! }
+//! # ;
+//! ```
+//!
+//! The example above declares a widget is is green when hovered or is holding the pointer capture, the widget also logs
+//! when it gets and loses the capture. Note that the [`gesture::is_cap_hovered`] state is not the same as [`gesture::is_hovered`],
+//! if changed to the second the example will not be green when not hovering, even though the widget still holds the capture, pointer
+//! capture changes the target of pointer events, but it does not mask the fact that the pointer is not actually over the widget.
+//!
+//! [`gesture::is_cap_hovered`]: fn@crate::gesture::is_cap_hovered
+//! [`gesture::is_hovered`]: fn@crate::gesture::is_hovered
+//!
 //! # Full API
 //!
 //! See [`zero_ui_ext_input::pointer_capture`] and [`zero_ui_wgt_input::pointer_capture`] for the full pointer capture API.
