398cbc85b7099ee3a05868d62a8b4db5a60cd978
Samuel Guerra <sam.rodr.g@gmail.com>
2024-01-22 23:45:10 -0300

    Added rule_line docs.

--- zero-ui/src/rule_line.rs
+++ zero-ui/src/rule_line.rs
@@ -1,5 +1,27 @@
 //! Rule line widgets and properties.
 //!
+//! A rule line is a horizontal or vertical separator line, this module provides 3 widgets the [`RuleLine!`](struct@RuleLine)
+//! base that can dynamically change orientation and the [`hr::Hr!`](struct@hr::Hr) and [`vr::Vr!`](struct@vr::Vr) that represents
+//! each orientation and can be styled separately.
+//!
+//! ```
+//! use zero_ui::prelude::*;
+//! # fn demo() {
+//!
+//! # let _ =
+//! Window! {
+//!     context_menu = ContextMenu!(ui_vec![
+//!         Button!(zero_ui::app::NEW_CMD.scoped(WINDOW.id())),
+//!         Button!(zero_ui::app::OPEN_CMD.scoped(WINDOW.id())),
+//!         Hr!(),
+//!         Button!(zero_ui::app::EXIT_CMD),
+//!     ]);
+//! }
+//! # ; }
+//! ```
+//!
+//! The example above uses the `Hr!` widget in a context menu to separate the commands into two groups.
+//!
 //! # Full API
 //!
 //! See [`zero_ui_wgt_rule_line`] for the full widget API.
