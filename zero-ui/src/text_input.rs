787aea14bcf24bf1dfec86512b35285a20ec5ef9
Samuel Guerra <sam.rodr.g@gmail.com>
2024-01-27 11:41:34 -0300

    Added text_input docs.

--- zero-ui/src/text_input.rs
+++ zero-ui/src/text_input.rs
@@ -1,8 +1,41 @@
 //! Text input widget and properties.
 //!
-//! The [`TextInput!`](struct@TextInput) widget is an text or value editor that is styleable.
+//! The [`TextInput!`](struct@TextInput) widget is an text or parsed value editor that is styleable.
+//! 
+//! The example below defines 3 text inputs with the [`FieldStyle!`](struct@FieldStyle).
 //!
-//!
+//! ```
+//! use zero_ui::prelude::*;
+//! # let _scope = APP.defaults();
+//! 
+//! # let _ =
+//! Stack! {
+//!     zero_ui::text_input::style_fn = style_fn!(|_| zero_ui::text_input::FieldStyle!());
+//!     children = ui_vec![
+//!         TextInput! {
+//!             txt = var(Txt::from("name"));
+//!             max_chars_count = 50;
+//!         },
+//!         TextInput! {
+//!             txt_parse = var(0u32);
+//!             zero_ui::text_input::field_help = "help text";
+//!             // txt_parse_on_stop = true;
+//!         },
+//!         TextInput! {
+//!             txt = var_from("pass");
+//!             obscure_txt = true;
+//!         },
+//!     ];
+//!     direction = StackDirection::top_to_bottom();
+//!     spacing = 5;
+//! }
+//! # ;
+//! ```
+//! 
+//! The first input binds directly to a `Txt` read-write variable. The second field binds to an `u32` read-write variable using the
+//! [`txt_parse`](struct@TextInput#txt_parse) property. The third field obscures the text. The `FieldStyle!` adds data validation 
+//! adorners to the `TextInput!`, in the first field a char count is shown, in the second field the [`field_help`](fn@field_help) 
+//! or parse errors are shown.
 //!
 //! # Full API
 //!
