ef3ca11e5bd34448623f5861e64976eef240edf7
Samuel Guerra <sam.rodr.g@gmail.com>
2024-01-29 15:33:27 -0300

    Added update docs.

--- zero-ui/src/update.rs
+++ zero-ui/src/update.rs
@@ -1,5 +1,27 @@
 //! App update service and types.
 //!
+//! The [`UPDATES`] service can execute arbitrary futures and setup update handlers. It can also be used to request update,
+//! info rebuild, layout and render for any widget. Note that from inside the widget you should use the [`WIDGET`] service instead,
+//! as it is more efficient.
+//!
+//! The example below setups a handler that is called every app update.
+//!
+//! ```
+//! use zero_ui::prelude::*;
+//!
+//! zero_ui::update::UPDATES
+//!   .on_pre_update(app_hn!(|args: &zero_ui::update::UpdateArgs, _| {
+//!       println!("pre_update #{}", args.count);
+//!   }))
+//!   .perm();
+//! ```
+//!
+//! Updates are coalesced, multiple requests for the same widget will cause it to only update once, and multiple widgets
+//! can update on the same pass. See the [Main Loop] docs in the `app` module for more details.
+//!
+//! [`WIDGET`]: crate::widget::WIDGET
+//! [Main Loop]: crate::app#main-loop
+//!
 //! # Full API
 //!
 //! See [`zero_ui_app::update`] for the full update API.
