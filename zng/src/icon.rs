b714a24940ef15c1cd66e3d5590076786524a960
Samuel Guerra <sam.rodr.g@gmail.com>
2024-04-07 16:26:25 -0300

    Bump crate version in docs.

--- zng/src/icon.rs
+++ zng/src/icon.rs
@@ -6,7 +6,7 @@
 //! [Material Design Icons]: https://github.com/google/material-design-icons
 //!
 //! ```toml
-//! zng = { version = "0.2.0", features = ["material_icons"] }
+//! zng = { version = "0.2.1", features = ["material_icons"] }
 //! ```
 //!
 //! ```
