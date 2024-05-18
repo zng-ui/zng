a9a2457230bec49b1f064feeb0c155f359da366e
Samuel Guerra <sam.rodr.g@gmail.com>
2024-05-18 12:29:43 -0300

    Add hot property demonstrator.

--- examples/hot-reload-lib/src/lib.rs
+++ examples/hot-reload-lib/src/lib.rs
@@ -3,29 +3,63 @@ use zng::{prelude::*, prelude_wgt::*};
 // Declare hot reload dynamic entry.
 zng::hot_reload::zng_hot_entry!();
 
+/// Hot reloading node.
 #[hot_node]
 pub fn hot_node() -> impl UiNode {
     tracing::info!("`hot_node()` called");
     Text! {
-        widget::on_init = hn!(|_|{
+        widget::on_init = hn!(|_| {
             tracing::info!("hot node on_init");
         });
-        widget::on_deinit = hn!(|_|{
+        widget::on_deinit = hn!(|_| {
             tracing::info!("hot node on_deinit");
         });
-        widget::background_color = rgb(255, 0, 0);
-        txt = "Hello, this node is hot!";
+        widget::background_color = rgb(0, 128, 255).darken(50.pct());
+        txt = "Hello this node is hot!";
     }
 }
 
-/*
-!!: ISSUES:
+/// Hot reloading property.
+///
+/// Note that the `input` does not hot reload, only changes inside the property. As an alternative
+/// you can declare a test `hot_node` that creates an widget that sets the property, in that context
+/// the property input can be hot reloaded.
+#[hot_node]
+#[property(FILL)]
+pub fn hot_prop(child: impl UiNode, input: impl IntoVar<bool>) -> impl UiNode {
+    let input = input.into_var();
 
-* Add mode demonstrators in example.
+    let mut clip = PxRect::zero();
 
-# Issues after merge
+    match_node(child, move |c, op| match op {
+        UiNodeOp::Init => {
+            WIDGET.sub_var_render(&input);
+        }
+        UiNodeOp::Layout { wl, final_size } => {
+            *final_size = c.layout(wl);
 
-* Tracing context did not bridge.
-* Implement panel to show status.
+            let s = Size::from(16).layout();
+            let mut offset = Align::TOP_RIGHT.child_offset(s, *final_size, LAYOUT.direction());
+            offset.x += s.width / 2;
+            offset.y -= s.height / 2;
+            let new_clip = PxRect::new(offset.to_point(), s);
+            if clip != new_clip {
+                clip = new_clip;
+                WIDGET.render();
+            }
+        }
+        UiNodeOp::Render { frame } => {
+            c.render(frame);
 
-*/
+            let color = match input.get() {
+                true => colors::GREEN,
+                false => colors::RED,
+            };
+
+            frame.push_clip_rounded_rect(clip, PxCornerRadius::new_all(clip.size), false, false, |frame| {
+                frame.push_color(clip, color.into());
+            });
+        }
+        _ => {}
+    })
+}
--- examples/hot_reload.rs
+++ examples/hot_reload.rs
@@ -21,15 +21,21 @@ fn app_main() {
         // used to run the program, it will rebuild only until the dylib is finished.
         zng::hot_reload::HOT_RELOAD.rebuilder(|a| a.build_example(Some("examples"), "hot_reload"));
 
+        let state = var(true);
+        let example = Container! {
+            // hot reloading node, edit the code in `examples/hot-reload-lib` to see updates.
+            child = examples_hot_reload::hot_node();
+
+            // hot reloading property, the state does not change, but changes in the property code update.
+            examples_hot_reload::hot_prop = state.clone();
+            gesture::on_click = hn!(|_| { state.set(!state.get()); });
+        };
+
         Window! {
             title = "Hot Reload Example";
-
             always_on_top = true;
 
-            child = Container! {
-                // hot reloading node, edit the code in `examples/hot-reload-lib` to see updates.
-                child = examples_hot_reload::hot_node();
-            };
+            child = example;
 
              // layout affects the hot node correctly.
             child_align = Align::CENTER;
