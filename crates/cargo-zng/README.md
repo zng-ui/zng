b6091e13603987ced463aa9027b490304c479a85
Samuel Guerra <sam.rodr.g@gmail.com>
2024-05-23 20:24:42 -0300

    Refactor cargo-zng-new.

--- crates/cargo-zng/README.md
+++ crates/cargo-zng/README.md
@@ -88,21 +88,19 @@ Also see [`zng::l10n::l10n!`] docs for more details about the expected format.
 
 Initialize a new repository from a Zng template repository
 
-Usage: cargo-zng.exe new [OPTIONS] <NAME>
+Usage: cargo zng new [OPTIONS] [VALUE]...
 
 Arguments:
-  <NAME>
-          Project Name
+  [VALUE]...
+          Set template values by position
 
-          Can be a simple "name" or a "qualifier/org/project-name".
+          The first value for all templates is the app name.
 
-          EXAMPLES
+          EXAMPLE
 
-          "br.com/My Org/My App" generates a `./my-app` project and sets metadata
+          cargo zng new "My App!" | creates a "my-app" project.
 
-          "my_app" generates a `./my_app` project
-
-          "My App" generates a `./my-app` project
+          cargo zng new "my_app"  | creates a "my_app" project.
 
 Options:
   -t, --template <TEMPLATE>
@@ -110,7 +108,17 @@ Options:
 
           Can be `.git` URL or an `owner/repo` for a GitHub repository.
 
+          Can also be an absolute path or `./path` to a local template directory.
+
           [default: zng-ui/zng-template]
+
+  -s, --set [<SET>...]
+          Set a template value
+
+          Templates have a `.zng-template` file that defines the possible options.
+
+  -k, --keys
+          Show all possible values that can be set on the template
 
   -h, --help
           Print help (see a summary with '-h')
@@ -119,25 +127,59 @@ Options:
           Print version
 ```
 
-Zng templates are very simple, it does not use any template engine, just Rust's string replace in UTF-8 text files only.
-The replacement keys are designed to be compilable, so template designers can build/check their template like a normal Rust project.
+Zng project generator is very simple, it does not use any template engine, just Rust's string replace in UTF-8 text files only.
+The replacement keys are compilable, so template designers can build/check their template like a normal Rust project.
 
-These are current supported keys, for an example name `"rs.qualifier/My Org/My App"`:
+Template keys encode the format they provide, these are the current supported key cases:
 
-##### In file/folder names and text
+* t-key-t — kebab-case
+* T-KEY-T — UPPER-KEBAB-CASE
+* t_key_t — snake_case
+* T_KEY_T — UPPER_SNAKE_CASE
+* T-Key-T — Train-Case
+* t.key.t — lower case
+* T.KEY.T — UPPER CASE
+* T.Key.T — Title Case
+* ttKeyTt — camelCase
+* TtKeyTt — PascalCase
+* {{key}} — Unchanged.
 
-* `t-app-t` — my-app
-* `t_app_t` — my_app
-* `T_APP_T` — MY_APP
-* `T-APP-T` — MY-APP
+The values for each format (except {{key}}) are cleaned of chars that do not match this pattern
+`[ascii_alphabetic][ascii_alphanumeric|'-'|'_'|' '|]*`. The case and separator conversions are applied to this
+cleaned value.
 
-##### Only in text
+The actual keys are declared by the template in a `.zng-template` file in the root of the template repository, they
+are ascii alphabetic with >=3 lowercase chars.
 
-* `t.App.t` — My App
-* `t-App-t` — My-App
-* `t.Org.t` — My Org
-* `t-Org-t` — My-Org
-* `t.qualifier.t` — rs.qualifier
+Call `cargo zng new --keys` to show help for the template keys.
+
+The default template has 3 keys:
+
+* `app` — The app name, the Cargo package and crate names are derived from it. Every template first key must be this one.
+* `org` — Used in `zng::env::init` as the 'organization' value.
+* `qualifier` — Used in `zng::env::init` as the 'qualifier' value.
+
+For an example input `cargo zng new "My App!" "My Org"` the template code:
+
+```rust
+// file: src/t_app_t_init.rs
+
+pub fn init_t_app_t() {
+    println!("init t-app-t");
+    zng::env::init("{{qualifier}}", "{{org}}", "{{app}}");
+}
+```
+
+Generates:
+
+```rust
+// file: src/my_app_init.rs
+
+pub fn init_my_app() {
+    println!("init my-app");
+    zng::env::init("", "My Org", "My App!");
+}
+```
 
 See [zng-ui/zng-template] for an example of templates.
 
--- crates/cargo-zng/src/new.rs
+++ crates/cargo-zng/src/new.rs
@@ -1,29 +1,29 @@
 //! Initialize a new repository from a Zng template repository
 
 use std::{
-    fs, io,
+    fs, io, mem,
     path::{Path, PathBuf},
 };
 
 use clap::*;
 use color_print::cstr;
+use convert_case::{Case, Casing};
 
 use crate::util;
 
 #[derive(Args, Debug)]
 pub struct NewArgs {
-    /// Project Name
+    /// Set template values by position
     ///
-    /// Can be a simple "name" or a "qualifier/org/project-name".
+    /// The first value for all templates is the app name.
     ///
-    /// EXAMPLES
+    /// EXAMPLE
     ///
-    /// "br.com/My Org/My App" generates a `./my-app` project and sets metadata
+    /// cargo zng new "My App!" | creates a "my-app" project.
     ///
-    /// "my_app" generates a `./my_app` project
-    ///
-    /// "My App" generates a `./my-app` project
-    name: String,
+    /// cargo zng new "my_app"  | creates a "my_app" project.
+    #[arg(num_args(0..))]
+    value: Vec<String>,
 
     /// Zng template
     ///
@@ -32,79 +32,165 @@ pub struct NewArgs {
     /// Can also be an absolute path or `./path` to a local template directory.
     #[arg(short, long, default_value = "zng-ui/zng-template")]
     template: String,
+
+    /// Set a template value
+    ///
+    /// Templates have a `.zng-template` file that defines the possible options.
+    #[arg(short, long, num_args(0..))]
+    set: Vec<String>,
+
+    /// Show all possible values that can be set on the template
+    #[arg(short, long, action)]
+    keys: bool,
 }
 
 pub fn run(args: NewArgs) {
-    let name = parse_name(args.name);
     let template = parse_template(args.template);
-    let package_name = name.package_name();
-    // let crate_name = name.crate_name();
 
-    println!(cstr!("<bold>validate name and init repo<bold>"));
-    if let Err(e) = util::cmd("cargo new --quiet --bin", &[package_name.as_str()], &[]) {
-        let _ = cleanup_cargo_new(&package_name);
+    if args.keys {
+        return print_keys(template);
+    }
+
+    let arg_keys = match parse_key_values(args.value, args.set) {
+        Ok(arg_keys) => {
+            if arg_keys.is_empty() || (!arg_keys[0].0.is_empty() && arg_keys.iter().all(|(k, _)| k != "app")) {
+                fatal!("missing required key `app`")
+            }
+            arg_keys
+        }
+        Err(e) => fatal!("{e}"),
+    };
+
+    println!(cstr!("<bold>validate name and init<bold>"));
+    let app = &arg_keys[0].1;
+    let project_name = clean_value(app, true)
+        .unwrap_or_else(|e| fatal!("{e}"))
+        .replace(' ', "-")
+        .to_lowercase();
+    if let Err(e) = util::cmd("cargo new --quiet --bin", &[project_name.as_str()], &[]) {
+        let _ = std::fs::remove_dir_all(&project_name);
         fatal!("{e}");
     }
 
-    if let Err(e) = cleanup_cargo_new(&package_name) {
+    if let Err(e) = cleanup_cargo_new(&project_name) {
         fatal!("failed to cleanup `cargo new` template, {e}");
     }
 
-    println!(cstr!("<bold>clone template to temp dir<bold>"));
-    let template_temp = format!("{package_name}.zng_template.tmp");
-    if let Err(e) = template.git_clone(&template_temp) {
-        fatal!("failed to clone template, {e}");
-    }
+    println!(cstr!("<bold>clone template<bold>"));
+    let template_temp = PathBuf::from(format!("{project_name}.zng_template.tmp"));
 
-    println!(cstr!("<bold>generate template<bold>"));
-    let cx = Fmt::new(&name);
-    if let Err(e) = apply_template(&cx, &template_temp, &package_name) {
-        error!("{e}");
+    let fatal_cleanup = || {
         let _ = fs::remove_dir_all(&template_temp);
-        let _ = fs::remove_dir_all(&package_name);
+        let _ = fs::remove_dir_all(&project_name);
+    };
+
+    let template_keys = template.git_clone(&template_temp, false).unwrap_or_else(|e| {
+        fatal_cleanup();
+        fatal!("failed to clone template, {e}")
+    });
+
+    let cx = Fmt::new(template_keys, arg_keys).unwrap_or_else(|e| {
+        fatal_cleanup();
+        fatal!("{e}")
+    });
+    println!(cstr!("<bold>generate template<bold>"));
+    if let Err(e) = apply_template(&cx, &template_temp, &project_name) {
+        error!("{e}");
+        fatal_cleanup();
         util::exit();
     }
-}
 
-struct Name {
-    qualifier: String,
-    org: String,
-    app: String,
-}
-impl Name {
-    fn package_name(&self) -> String {
-        self.app.replace(' ', "-").to_lowercase()
-    }
-
-    fn crate_name(&self) -> String {
-        self.package_name().replace('-', "_")
+    println!(cstr!("<bold>cargo fmt<bold>"));
+    if let Err(e) = std::env::set_current_dir(project_name).and_then(|_| util::cmd("cargo fmt", &[], &[])) {
+        fatal!("{e}")
     }
 }
 
-fn parse_name(arg: String) -> Name {
-    let arg: Box<[_]> = arg.splitn(3, '/').map(|s| s.trim()).collect();
-    let qualifier;
-    let org;
-    let app;
-    if arg.len() == 3 {
-        qualifier = arg[0];
-        org = arg[1];
-        app = arg[2];
-    } else {
-        qualifier = "";
-        org = "";
-        app = arg[0];
+fn clean_value(value: &str, required: bool) -> io::Result<String> {
+    let mut first_char = false;
+    let clean_value: String = value
+        .chars()
+        .filter(|c| {
+            if first_char {
+                first_char = c.is_ascii_alphabetic();
+                first_char
+            } else {
+                *c == ' ' || *c == '-' || *c == '_' || c.is_ascii_alphanumeric()
+            }
+        })
+        .collect();
+    let clean_value = clean_value.trim().to_owned();
+
+    if required && clean_value.is_empty() {
+        if clean_value.is_empty() {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                format!("cannot derive clean value from `{value}`, must contain at least one ascii alphabetic char"),
+            ));
+        }
+        if clean_value.len() > 62 {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                format!("cannot derive clean value from `{value}`, must contain <= 62 ascii alphanumeric chars"),
+            ));
+        }
+    }
+    Ok(clean_value)
+}
+
+fn parse_key_values(value: Vec<String>, define: Vec<String>) -> io::Result<ArgsKeyMap> {
+    let mut r = Vec::with_capacity(value.len() + define.len());
+
+    for value in value {
+        r.push((String::new(), value));
     }
 
-    if arg.len() == 2 || app.contains('/') {
-        fatal!(r#"NAME must be a "name" or a "qualifier/organization/application""#);
+    for key_value in define {
+        if let Some((key, value)) = key_value.split_once('=') {
+            if !is_key(key) {
+                return Err(io::Error::new(io::ErrorKind::InvalidInput, format!("invalid key `{key}`")));
+            }
+            r.push((key.to_owned(), value.to_owned()));
+        }
     }
 
-    Name {
-        qualifier: qualifier.to_owned(),
-        org: org.to_owned(),
-        app: app.to_owned(),
+    Ok(r)
+}
+
+fn print_keys(template: Template) {
+    println!(cstr!("<bold>clone template to temp dir<bold>"));
+
+    for i in 0..100 {
+        let template_temp = std::env::temp_dir().join(format!("cargo-zng-template-keys-help-{i}"));
+        if template_temp.exists() {
+            continue;
+        }
+
+        match template.git_clone(&template_temp, true) {
+            Ok(keys) => {
+                println!("TEMPLATE KEYS\n");
+                for kv in keys {
+                    let value = match &kv.value {
+                        Some(dft) => dft.as_str(),
+                        None => cstr!("<bold><y>required</y></bold>"),
+                    };
+                    println!(cstr!("<bold>{}=</bold>{}"), kv.key, value);
+                    if !kv.docs.is_empty() {
+                        for line in kv.docs.lines() {
+                            println!("   {line}");
+                        }
+                        println!();
+                    }
+                }
+            }
+            Err(e) => {
+                error!("failed to clone template, {e}");
+            }
+        }
+        let _ = fs::remove_dir_all(&template_temp);
+        return;
     }
+    fatal!("failed to clone template, no temp dir available");
 }
 
 fn parse_template(arg: String) -> Template {
@@ -135,7 +221,8 @@ enum Template {
     Local(PathBuf),
 }
 impl Template {
-    fn git_clone(self, to: &str) -> io::Result<()> {
+    /// Clone repository, if it is a template return the `.zng-template` file contents.
+    fn git_clone(self, to: &Path, include_docs: bool) -> io::Result<KeyMap> {
         let from = match self {
             Template::Git(url) => url,
             Template::Local(path) => {
@@ -153,7 +240,20 @@ impl Template {
                 }
             }
         };
-        util::cmd("git clone --depth 1", &[from.as_str(), to], &[])
+        util::cmd("git clone --depth 1", &[from.as_str(), &to.display().to_string()], &[])?;
+
+        match fs::read_to_string(PathBuf::from(to).join(".zng-template")) {
+            Ok(s) => parse_keys(s, include_docs),
+            Err(e) => {
+                if e.kind() == io::ErrorKind::NotFound {
+                    return Err(io::Error::new(
+                        io::ErrorKind::NotFound,
+                        "git repo is not a zng template, missing `.zng-template`",
+                    ));
+                }
+                Err(e)
+            }
+        }
     }
 }
 
@@ -172,14 +272,40 @@ fn cleanup_cargo_new(path: &str) -> io::Result<()> {
     Ok(())
 }
 
-fn apply_template(cx: &Fmt, template_temp: &str, package_name: &str) -> io::Result<()> {
-    let template_temp = PathBuf::from(template_temp);
-    // remove template .git
+fn apply_template(cx: &Fmt, template_temp: &Path, package_name: &str) -> io::Result<()> {
+    // remove template .git, .zng-template and Cargo.lock
     fs::remove_dir_all(template_temp.join(".git"))?;
+    fs::remove_file(template_temp.join(".zng-template"))?;
+
+    // remove .zng-template-ignore
+    let can_temp = template_temp.canonicalize()?;
+    match fs::read_to_string(template_temp.join(".zng-template-ignore")) {
+        Ok(ignore) => {
+            for glob in ignore.lines().map(|l| l.trim()).filter(|l| !l.is_empty() && !l.starts_with('#')) {
+                for path in glob::glob(glob).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))? {
+                    let path = path.map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?.canonicalize()?;
+                    if path.starts_with(&can_temp) {
+                        if path.is_dir() {
+                            fs::remove_dir_all(path)?;
+                        } else if path.is_file() {
+                            fs::remove_file(path)?;
+                        }
+                    }
+                }
+            }
+        }
+        Err(e) => {
+            if e.kind() != io::ErrorKind::NotFound {
+                return Err(e);
+            }
+        }
+    }
+    fs::remove_file(template_temp.join(".zng-template-ignore"))?;
+
     // rename/rewrite template and move it to new package dir
-    apply(cx, &template_temp, &PathBuf::from(package_name))?;
+    apply(cx, template_temp, &PathBuf::from(package_name))?;
     // remove (empty) template temp
-    fs::remove_dir_all(&template_temp)
+    fs::remove_dir_all(template_temp)
 }
 
 fn apply(cx: &Fmt, from: &Path, to: &Path) -> io::Result<()> {
@@ -203,31 +329,36 @@ fn apply(cx: &Fmt, from: &Path, to: &Path) -> io::Result<()> {
 }
 
 struct Fmt {
-    rename: Vec<(&'static str, String)>,
-    rewrite: Vec<(&'static str, String)>,
+    replace: ReplaceMap,
 }
 impl Fmt {
-    fn new(name: &Name) -> Self {
-        let rename = vec![
-            ("t-app-t", name.package_name()),
-            ("t_app_t", name.crate_name()),
-            ("T_APP_T", name.crate_name().to_uppercase()),
-            ("T-APP-T", name.package_name().to_uppercase()),
-        ];
-        let rewrite = vec![
-            ("t.App.t", name.app.clone()),
-            ("t-App-t", name.app.replace(' ', "-")),
-            ("t.Org.t", name.org.clone()),
-            ("t-Org-t", name.org.replace(' ', "-")),
-            ("t.qualifier.t", name.qualifier.clone()),
-        ];
-
-        Self { rename, rewrite }
+    fn new(mut template_keys: KeyMap, arg_keys: ArgsKeyMap) -> io::Result<Self> {
+        for (i, (key, value)) in arg_keys.into_iter().enumerate() {
+            if key.is_empty() {
+                if i >= template_keys.len() {
+                    return Err(io::Error::new(
+                        io::ErrorKind::InvalidInput,
+                        "more positional values them template keys",
+                    ));
+                }
+                template_keys[i].value = Some(value);
+            } else if let Some(kv) = template_keys.iter_mut().find(|kv| kv.key == key) {
+                kv.value = Some(value);
+            } else {
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    format!("unknown key `{key}`, not declared by template"),
+                ));
+            }
+        }
+        Ok(Self {
+            replace: make_replacements(&template_keys)?,
+        })
     }
 
     fn rename(&self, template_path: &Path) -> io::Result<PathBuf> {
         let mut path = template_path.to_string_lossy().into_owned();
-        for (key, value) in &self.rename {
+        for (key, value) in &self.replace {
             path = path.replace(key, value);
         }
         let path = PathBuf::from(path);
@@ -241,7 +372,7 @@ impl Fmt {
         match fs::read_to_string(template_path) {
             Ok(txt) => {
                 let mut new_txt = txt.clone();
-                for (key, value) in self.rename.iter().chain(&self.rewrite) {
+                for (key, value) in &self.replace {
                     new_txt = new_txt.replace(key, value);
                 }
                 if new_txt != txt {
@@ -259,4 +390,123 @@ impl Fmt {
             }
         }
     }
+}
+
+static PATTERNS: &[(&str, &str, Option<Case>)] = &[
+    ("t-key-t", "kebab-case", Some(Case::Kebab)),
+    ("T-KEY-T", "UPPER-KEBAB-CASE", Some(Case::UpperFlat)),
+    ("t_key_t", "snake_case", Some(Case::Snake)),
+    ("T_KEY_T", "UPPER_SNAKE_CASE", Some(Case::UpperSnake)),
+    ("T-Key-T", "Train-Case", Some(Case::Train)),
+    ("t.key.t", "lower case", Some(Case::Lower)),
+    ("T.KEY.T", "UPPER CASE", Some(Case::Upper)),
+    ("T.Key.T", "Title Case", Some(Case::Title)),
+    ("ttKeyTt", "camelCase", Some(Case::Camel)),
+    ("TtKeyTt", "PascalCase", Some(Case::Pascal)),
+    ("{{key}}", "<unchanged>", None),
+];
+
+type KeyMap = Vec<TemplateKey>;
+type ArgsKeyMap = Vec<(String, String)>;
+type ReplaceMap = Vec<(String, String)>;
+
+struct TemplateKey {
+    docs: String,
+    key: String,
+    value: Option<String>,
+    required: bool,
+}
+
+fn is_key(s: &str) -> bool {
+    s.len() >= 3 && s.is_ascii() && s.chars().all(|c| c.is_ascii_alphabetic() && c.is_lowercase())
+}
+
+fn parse_keys(zng_template_v1: String, include_docs: bool) -> io::Result<KeyMap> {
+    let mut r = vec![];
+
+    let mut docs = String::new();
+
+    for (i, line) in zng_template_v1.lines().enumerate() {
+        let line = line.trim();
+        if line.is_empty() {
+            docs.clear();
+            continue;
+        }
+
+        if line.starts_with('#') {
+            if include_docs {
+                let mut line = line.trim_start_matches('#');
+                if line.starts_with(' ') {
+                    line = &line[1..];
+                }
+                docs.push_str(dbg!(line));
+                docs.push('\n');
+            }
+            continue;
+        }
+
+        if r.is_empty() && line != "app=" {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "broken template, first key must be `app=`",
+            ));
+        }
+
+        let docs = mem::take(&mut docs);
+        if let Some((key, val)) = line.split_once('=') {
+            if is_key(key) {
+                if val.is_empty() {
+                    r.push(TemplateKey {
+                        docs,
+                        key: key.to_owned(),
+                        value: None,
+                        required: true,
+                    });
+                    continue;
+                } else if val.starts_with('"') && val.ends_with('"') {
+                    r.push(TemplateKey {
+                        docs,
+                        key: key.to_owned(),
+                        value: Some(val[1..val.len() - 1].to_owned()),
+                        required: false,
+                    });
+                    continue;
+                }
+            }
+        }
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidData,
+            format!("broken template, invalid syntax in `.zng-template:{}`", i + 1),
+        ));
+    }
+
+    Ok(r)
+}
+fn make_replacements(keys: &KeyMap) -> io::Result<ReplaceMap> {
+    let mut r = Vec::with_capacity(keys.len() * PATTERNS.len());
+    for kv in keys {
+        let value = match &kv.value {
+            Some(v) => v,
+            None => {
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    format!("missing required key `{}`", kv.key),
+                ))
+            }
+        };
+        let clean_value = clean_value(value, kv.required)?;
+
+        for (pattern, _, case) in PATTERNS {
+            let prefix = &pattern[..2];
+            let suffix = &pattern[pattern.len() - 2..];
+            let (key, value) = if let Some(case) = case {
+                (kv.key.to_case(*case), clean_value.to_case(*case))
+            } else {
+                (kv.key.to_owned(), value.to_owned())
+            };
+            let key = format!("{prefix}{key}{suffix}");
+            r.push((key, value));
+        }
+    }
+    Ok(r)
 }
