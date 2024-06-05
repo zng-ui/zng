6201aef92f07cf79a47508e85b9c281efd8dac10
Samuel <sam.rodr.g@gmail.com>
2024-06-05 17:07:44 -0300

    Impl config from dconf (#267)

---
+++ crates/zng-view/src/config/dconf.rs
@@ -0,0 +1,169 @@
+use std::{io::BufRead as _, sync::Arc, time::Duration};
+
+use zng_unit::TimeUnits as _;
+use zng_view_api::{
+    config::{AnimationsConfig, ColorScheme, FontAntiAliasing, KeyRepeatConfig, LocaleConfig, MultiClickConfig, TouchConfig},
+    Event,
+};
+
+use crate::AppEvent;
+
+pub fn font_aa() -> FontAntiAliasing {
+    super::other::font_aa()
+}
+
+pub fn multi_click_config() -> MultiClickConfig {
+    let mut cfg = MultiClickConfig::default();
+    if let Some(d) = dconf_uint("/org/gnome/desktop/peripherals/mouse/double-click") {
+        cfg.time = d.ms();
+    }
+    cfg
+}
+
+pub fn animations_config() -> AnimationsConfig {
+    let mut cfg = AnimationsConfig::default();
+    if let Some(e) = dconf_bool("/org/gnome/desktop/interface/enable-animations") {
+        cfg.enabled = e;
+    }
+    if let Some(d) = dconf_uint("/org/gnome/desktop/interface/cursor-blink-time") {
+        cfg.caret_blink_interval = d.ms();
+    }
+    if let Some(e) = dconf_bool("/org/gnome/desktop/interface/cursor-blink") {
+        if !e {
+            cfg.caret_blink_interval = Duration::MAX;
+        }
+    }
+    cfg
+}
+
+pub fn key_repeat_config() -> KeyRepeatConfig {
+    let mut cfg = KeyRepeatConfig::default();
+    if let Some(d) = dconf_uint("/org/gnome/desktop/peripherals/keyboard/delay") {
+        cfg.start_delay = d.ms();
+    }
+    if let Some(d) = dconf_uint("/org/gnome/desktop/peripherals/keyboard/repeat-interval") {
+        cfg.interval = d.ms();
+    }
+    cfg
+}
+
+pub fn touch_config() -> TouchConfig {
+    super::other::touch_config()
+}
+
+pub fn color_scheme_config() -> ColorScheme {
+    match dconf("/org/gnome/desktop/interface/color-scheme") {
+        Some(cs) => {
+            if cs.contains("dark") {
+                ColorScheme::Dark
+            } else {
+                ColorScheme::Light
+            }
+        }
+        None => ColorScheme::Light,
+    }
+}
+
+pub fn locale_config() -> LocaleConfig {
+    // sys_locale
+    super::other::locale_config()
+}
+
+fn on_change(key: &str, s: &crate::AppEventSender) {
+    println!("{key}"); // to discover keys, uncomment and change the config in system config app.
+
+    match key {
+        "/org/gnome/desktop/interface/color-scheme" => {
+            let _ = s.send(AppEvent::ColorSchemeConfigChanged);
+        }
+        "/org/gnome/desktop/peripherals/keyboard/delay" | "/org/gnome/desktop/peripherals/keyboard/repeat-interval" => {
+            let _ = s.send(AppEvent::Notify(Event::KeyRepeatConfigChanged(key_repeat_config())));
+        }
+        "/org/gnome/desktop/peripherals/mouse/double-click" => {
+            let _ = s.send(AppEvent::Notify(Event::MultiClickConfigChanged(multi_click_config())));
+        }
+        "/org/gnome/desktop/interface/enable-animations"
+        | "/org/gnome/desktop/interface/cursor-blink-time"
+        | "/org/gnome/desktop/interface/cursor-blink" => {
+            let _ = s.send(AppEvent::Notify(Event::AnimationsConfigChanged(animations_config())));
+        }
+        _ => {}
+    }
+}
+
+fn dconf_bool(key: &str) -> Option<bool> {
+    let s = dconf(key)?;
+    match s.parse::<bool>() {
+        Ok(b) => Some(b),
+        Err(e) => {
+            tracing::error!("unexpected value for {key} '{s}', parse error: {e}");
+            None
+        }
+    }
+}
+
+fn dconf_uint(key: &str) -> Option<u64> {
+    let s = dconf(key)?;
+    if let Some((_, i)) = s.rsplit_once(' ') {
+        match i.parse::<u64>() {
+            Ok(i) => Some(i),
+            Err(e) => {
+                tracing::error!("unexpected value for {key} '{i}', parse error: {e}");
+                None
+            }
+        }
+    } else {
+        tracing::error!("unexpected value for {key} '{s}'");
+        None
+    }
+}
+
+fn dconf(key: &str) -> Option<String> {
+    let out = std::process::Command::new("dconf").arg("read").arg(key).output();
+    match out {
+        Ok(s) => {
+            if s.status.success() {
+                Some(String::from_utf8_lossy(&s.stdout).trim().to_owned())
+            } else {
+                let e = String::from_utf8_lossy(&s.stderr);
+                tracing::error!("dconf read {key} error, {}", e.lines().next().unwrap_or_default());
+                None
+            }
+        }
+        Err(e) => {
+            tracing::error!("cannot run dconf, {e}");
+            None
+        }
+    }
+}
+
+pub fn spawn_listener(event_loop: crate::AppEventSender) -> Option<Box<dyn FnOnce()>> {
+    let w = match duct::cmd!("dconf", "watch", "/").reader() {
+        Ok(w) => w,
+        Err(e) => {
+            tracing::error!("cannot monitor config, dconf did not spawn, {e}");
+            return None;
+        }
+    };
+    let w = Arc::new(w);
+    let ww = w.clone();
+
+    std::thread::spawn(move || {
+        for line in std::io::BufReader::new(&*w).lines() {
+            match line {
+                Ok(l) => {
+                    if l.starts_with('/') {
+                        on_change(&l, &event_loop);
+                    }
+                }
+                Err(_) => {
+                    break;
+                }
+            }
+        }
+    });
+
+    Some(Box::new(move || {
+        let _ = ww.kill();
+    }))
+}
---
+++ crates/zng-view/src/config/other.rs
@@ -0,0 +1,49 @@
+#![allow(unused)]
+
+use zng_view_api::config::{AnimationsConfig, ColorScheme, FontAntiAliasing, KeyRepeatConfig, MultiClickConfig, TouchConfig};
+
+pub fn font_aa() -> FontAntiAliasing {
+    warn("font_aa");
+    FontAntiAliasing::Subpixel
+}
+
+pub fn multi_click_config() -> MultiClickConfig {
+    warn("multi_click_config");
+    MultiClickConfig::default()
+}
+
+pub fn animations_config() -> AnimationsConfig {
+    warn("animations_config");
+    AnimationsConfig::default()
+}
+
+pub fn key_repeat_config() -> KeyRepeatConfig {
+    warn("key_repeat_config");
+    KeyRepeatConfig::default()
+}
+
+pub fn touch_config() -> TouchConfig {
+    warn("touch_config");
+    TouchConfig::default()
+}
+
+pub fn color_scheme_config() -> ColorScheme {
+    warn("color_scheme_config");
+    ColorScheme::default()
+}
+
+#[cfg(not(windows))]
+pub fn locale_config() -> zng_view_api::config::LocaleConfig {
+    zng_view_api::config::LocaleConfig {
+        langs: sys_locale::get_locale().into_iter().map(zng_txt::Txt::from).collect(),
+    }
+}
+
+pub fn spawn_listener(_: crate::AppEventSender) -> Option<Box<dyn FnOnce()>> {
+    tracing::warn!("config events not implemented for {}", std::env::consts::OS);
+    None
+}
+
+fn warn(name: &str) {
+    tracing::warn!("system '{name}' not implemented for {}", std::env::consts::OS);
+}
