2fe6c12c49fc54fc106d13f214aa738cd218bdb1
Samuel <sam.rodr.g@gmail.com>
2024-09-21 15:40:58 -0300

    Bump patch changes.

--- crates/zng/src/app.rs
+++ crates/zng/src/app.rs
@@ -23,7 +23,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! zng = { version = "0.11.8", features = ["view_prebuilt"] }
+//! zng = { version = "0.11.9", features = ["view_prebuilt"] }
 //! ```
 //!
 //! ```no_run
