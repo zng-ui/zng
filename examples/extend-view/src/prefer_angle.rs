34f679483ee4c696f3afdb0596f180aa96d308ee
Samuel Guerra <sam.rodr.g@gmail.com>
2024-10-08 01:41:24 -0300

    fmt

--- examples/extend-view/src/prefer_angle.rs
+++ examples/extend-view/src/prefer_angle.rs
@@ -18,11 +18,13 @@ pub fn extension_id() -> ApiExtensionId {
 #[property(CONTEXT)]
 pub fn use_angle_egl(child: impl UiNode, enable: impl IntoValue<bool>) -> impl UiNode {
     let enable = enable.into() && cfg!(windows);
-    match_node(child, move |_c, op| if let UiNodeOp::Init = op {
-        if enable {
-            WINDOWS
-                .view_extensions_init(WINDOW.id(), extension_id(), ApiExtensionPayload::serialize(&true).unwrap())
-                .unwrap();
+    match_node(child, move |_c, op| {
+        if let UiNodeOp::Init = op {
+            if enable {
+                WINDOWS
+                    .view_extensions_init(WINDOW.id(), extension_id(), ApiExtensionPayload::serialize(&true).unwrap())
+                    .unwrap();
+            }
         }
     })
 }
