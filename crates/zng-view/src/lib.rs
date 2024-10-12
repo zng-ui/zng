253d7e3e1ecaa0b9e478cb8d1d47e50c14cc405f
Samuel <sam.rodr.g@gmail.com>
2024-10-12 14:48:32 -0300

    Bump minor

--- crates/zng-view/src/lib.rs
+++ crates/zng-view/src/lib.rs
@@ -12,7 +12,7 @@
 //! ```toml
 //! [dependencies]
 //! zng = "0.12.3"
-//! zng-view = "0.5.1"
+//! zng-view = "0.6.0"
 //! ```
 //!
 //! Then call `zng::env::init` before any other code in `main` to setup a view-process that uses
