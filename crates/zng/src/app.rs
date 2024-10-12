610018d09591d6f03b4430b6e677aeeaa7c66784
Samuel <sam.rodr.g@gmail.com>
2024-10-12 14:48:13 -0300

    Bump patch

--- crates/zng/src/app.rs
+++ crates/zng/src/app.rs
@@ -23,7 +23,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! zng = { version = "0.12.2", features = ["view_prebuilt"] }
+//! zng = { version = "0.12.3", features = ["view_prebuilt"] }
 //! ```
 //!
 //! ```no_run
--- crates/zng/src/lib.rs
+++ crates/zng/src/lib.rs
@@ -17,7 +17,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! zng = { version = "0.12.2", features = ["view_prebuilt"] }
+//! zng = { version = "0.12.3", features = ["view_prebuilt"] }
 //! ```
 //!
 //! Then create your first app:
