605a4117336f4197db0cc06f74522bb0dfa472ad
Samuel <sam.rodr.g@gmail.com>
2024-10-30 16:04:12 -0300

    Add  `zng-wgt-progress` that implements progress indicator widgets

---
+++ crates/zng-wgt-progress/src/lib.rs
@@ -0,0 +1,20 @@
+#![doc(html_favicon_url = "https://raw.githubusercontent.com/zng-ui/zng/main/examples/image/res/zng-logo-icon.png")]
+#![doc(html_logo_url = "https://raw.githubusercontent.com/zng-ui/zng/main/examples/image/res/zng-logo.png")]
+//!
+//! Progress indicator widget.
+//!
+//! # Crate
+//!
+#![doc = include_str!(concat!("../", std::env!("CARGO_PKG_README")))]
+#![warn(unused_extern_crates)]
+#![warn(missing_docs)]
+
+use zng_wgt::prelude::*;
+
+/// Progress indicator widget.
+#[widget($crate::Progress {
+    ($status:expr) => {
+        status = $value;
+    };
+})]
+pub struct Progress(WidgetBase);
