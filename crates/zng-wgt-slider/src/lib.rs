4669ce9053d6ddae47d822e64dda27f012473747
Samuel <sam.rodr.g@gmail.com>
2024-11-07 23:55:45 -0300

    Improve slider Selector

--- crates/zng-wgt-slider/src/lib.rs
+++ crates/zng-wgt-slider/src/lib.rs
@@ -71,7 +71,20 @@ impl<T, Tf: Fn(&T) -> Factor + Send, Ff: Fn(Factor) -> T + Send> OffsetConvert<T
     }
 }
 
+/// Represents a type that can auto implement a [`Selector`].
+///
+/// # Implementing
+///
+/// This trait is implemented for all primitive type and Zng layout types, if a type does not you
+/// can declare custom conversions using [`Selector::single`].
+pub trait SelectorValue: VarValue {
+    /// Make the selector.
+    fn to_selector(value: BoxedVar<Self>, min: Self, max: Self) -> Selector;
+}
+
 /// Defines the values and ranges selected by a slider.
+///
+/// Selectors are set on the [`selector`](fn@selector) property.
 #[derive(Clone)]
 pub struct Selector(Arc<Mutex<dyn SelectorImpl>>);
 impl fmt::Debug for Selector {
@@ -85,11 +98,16 @@ impl PartialEq for Selector {
     }
 }
 impl Selector {
+    /// New with single value thumb of type `T` that can be set any value in the `min..=max` range.
+    pub fn value<T: SelectorValue>(selection: impl IntoVar<T>, min: T, max: T) -> Self {
+        T::to_selector(selection.into_var().boxed(), min, max)
+    }
+
     /// New with a single value thumb of type `T`.
     ///
     /// The value must convert to a normalized factor `[0.fct()..=1.fct()]` where `0.fct()` is the minimum possible value and `1.fct()` is the maximum
     /// possible value. If a value outside of this range is returned it is clamped to the range and the `selection` variable is updated back.
-    pub fn single<T>(
+    pub fn value_with<T>(
         selection: impl IntoVar<T>,
         to_offset: impl Fn(&T) -> Factor + Send + 'static,
         from_offset: impl Fn(Factor) -> T + Send + 'static,
@@ -136,12 +154,21 @@ impl Selector {
         })))
     }
 
+    /// New with two value thumbs of type `T` that can be set any value in the `min..=max` range.
+    pub fn range<T: SelectorValue>(range: impl IntoVar<std::ops::Range<T>>, min: T, max: T) -> Self {
+        // create a selector just to get the conversion closures
+        let convert = T::to_selector(zng_var::LocalVar(min.clone()).boxed(), min, max);
+        Self::range_with(range.into_var(), clmv!(convert, |t| convert.to_offset(t).unwrap()), move |f| {
+            convert.from_offset(f).unwrap()
+        })
+    }
+
     /// New with two values thumbs that define a range of type `T`.
     ///
     /// The conversion closure have the same constraints as [`single`].
     ///
     /// [`single`]: Self::single
-    pub fn range<T>(
+    pub fn range_with<T>(
         range: impl IntoVar<std::ops::Range<T>>,
         to_offset: impl Fn(&T) -> Factor + Send + 'static,
         from_offset: impl Fn(Factor) -> T + Send + 'static,
@@ -204,12 +231,21 @@ impl Selector {
         })))
     }
 
+    /// New with many value thumbs of type `T` that can be set any value in the `min..=max` range.
+    pub fn many<T: SelectorValue>(many: impl IntoVar<Vec<T>>, min: T, max: T) -> Self {
+        // create a selector just to get the conversion closures
+        let convert = T::to_selector(zng_var::LocalVar(min.clone()).boxed(), min, max);
+        Self::many_with(many.into_var(), clmv!(convert, |t| convert.to_offset(t).unwrap()), move |f| {
+            convert.from_offset(f).unwrap()
+        })
+    }
+
     /// New with many value thumbs of type `T`.
     ///
     /// The conversion closure have the same constraints as [`single`].
     ///
     /// [`single`]: Self::single
-    pub fn many<T>(
+    pub fn many_with<T>(
         many: impl IntoVar<Vec<T>>,
         to_offset: impl Fn(&T) -> Factor + Send + 'static,
         from_offset: impl Fn(Factor) -> T + Send + 'static,
@@ -273,7 +309,7 @@ impl Selector {
 
     /// New with no value thumb.
     pub fn nil() -> Self {
-        Self::many(vec![], |_: &bool| 0.fct(), |_| false)
+        Self::many_with(vec![], |_: &bool| 0.fct(), |_| false)
     }
 
     /// Convert the value to a normalized factor.
@@ -545,4 +581,140 @@ fn slider_track_node() -> impl UiNode {
         }
         op => thumbs.op(op),
     })
+}
+
+macro_rules! impl_32 {
+    (($to_f32:expr, $from_f32:expr) => $($T:ident),+ $(,)?) => {
+        $(
+            impl SelectorValue for $T {
+                #[allow(clippy::unnecessary_cast)]
+                fn to_selector(value: BoxedVar<Self>, min: Self, max: Self) -> Selector {
+                    let to_f32 = $to_f32;
+                    let from_f32 = $from_f32;
+
+                    let min = to_f32(min);
+                    let max = to_f32(max);
+                    if min >= max {
+                        Selector::nil()
+                    } else {
+                        let d = max - min;
+                        Selector::value_with(value, move |i| {
+                            let i = to_f32(i.clone());
+                            ((i - min)  / d).fct()
+                        }, move |f| {
+                            from_f32((f.0 * d + min).round())
+                        })
+                    }
+                }
+            }
+        )+
+    };
+}
+impl_32!((|i| i as f32, |f| f as Self) => u32, i32, u16, i16, u8, i8, f32);
+impl_32!((|p: Self| p.0 as f32, |f| Self(f as _)) => Px, Factor, FactorPercent);
+impl_32!((|p: Dip| p.to_f32(), |f| Dip::new_f32(f)) => Dip);
+
+macro_rules! impl_64 {
+    ($($T:ident),+ $(,)?) => {
+        $(
+            impl SelectorValue for $T {
+                fn to_selector(value: BoxedVar<Self>, min: Self, max: Self) -> Selector {
+                    let min = min as f64;
+                    let max = max as f64;
+                    if min >= max {
+                        Selector::nil()
+                    } else {
+                        let d = max - min;
+                        Selector::value_with(value, move |&i| {
+                            let i = i as f64;
+                            Factor(((i - min)  / d) as f32)
+                        }, move |f| {
+                            ((f.0 as f64) * d + min).round() as Self
+                        })
+                    }
+                }
+            }
+        )+
+    };
+}
+impl_64!(u64, i64, u128, i128, f64);
+
+impl SelectorValue for Length {
+    fn to_selector(value: BoxedVar<Self>, min: Self, max: Self) -> Selector {
+        let (min_f32, max_f32) = LAYOUT.with_context(LayoutMetrics::new(1.fct(), PxSize::splat(Px(1000)), Px(16)), || {
+            (min.layout_f32(LayoutAxis::X), max.layout_f32(LayoutAxis::X))
+        });
+        if min_f32 >= max_f32 {
+            Selector::nil()
+        } else {
+            let d_f32 = max_f32 - min_f32;
+            let d = max - min.clone();
+            Selector::value_with(
+                value,
+                move |l| {
+                    let l = LAYOUT.with_context(LayoutMetrics::new(1.fct(), PxSize::splat(Px(1000)), Px(16)), || {
+                        l.layout_f32(LayoutAxis::X)
+                    });
+                    Factor((l - min_f32) / d_f32)
+                },
+                move |f| d.clone() * f + min.clone(),
+            )
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn selector_value_t<T: SelectorValue>(min: T, max: T) {
+        let s = Selector::value(var(min.clone()), min.clone(), max.clone());
+        assert_eq!(s.to_offset(&min), Some(0.fct()));
+        assert_eq!(s.to_offset(&max), Some(1.fct()));
+        assert_eq!(s.from_offset(0.fct()), Some(min));
+        assert_eq!(s.from_offset(1.fct()), Some(max));
+    }
+
+    #[test]
+    fn selector_value_u8() {
+        selector_value_t(u8::MIN, u8::MAX);
+        selector_value_t(20u8, 120u8);
+    }
+
+    #[test]
+    fn selector_value_i32() {
+        selector_value_t(i32::MIN, i32::MAX);
+        selector_value_t(20i32, 120i32);
+    }
+
+    #[test]
+    fn selector_value_i64() {
+        selector_value_t(i64::MIN, i64::MAX);
+    }
+
+    #[test]
+    fn selector_value_f64() {
+        selector_value_t(-200f64, 200f64);
+        selector_value_t(20f64, 120f64);
+    }
+
+    #[test]
+    fn selector_pct() {
+        selector_value_t(0.pct(), 100.pct());
+    }
+
+    #[test]
+    fn selector_value_px() {
+        selector_value_t(Px(20), Px(200));
+    }
+
+    #[test]
+    fn selector_value_dip() {
+        selector_value_t(Dip::new(20), Dip::new(200));
+    }
+
+    #[test]
+    fn selector_value_length() {
+        selector_value_t(20.px(), 200.px());
+    }
 }
--- crates/zng/src/slider.rs
+++ crates/zng/src/slider.rs
@@ -10,7 +10,7 @@
 //!
 //! See [`zng_wgt_slider`] for the full widget API.
 
-pub use zng_wgt_slider::{DefaultStyle, Selector, Slider, SliderDirection, SliderTrack, ThumbArgs, SLIDER_DIRECTION_VAR};
+pub use zng_wgt_slider::{DefaultStyle, Selector, SelectorValue, Slider, SliderDirection, SliderTrack, ThumbArgs, SLIDER_DIRECTION_VAR};
 
 /// Slider thumb widget, styles and properties.
 ///
