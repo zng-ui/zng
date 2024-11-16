a0281a1706607db5b525f9a5f0aa99db1671b34c
Samuel <sam.rodr.g@gmail.com>
2024-11-16 18:59:20 -0300

    Publish 0.12.9

--- crates/zng/src/lib.rs
+++ crates/zng/src/lib.rs
@@ -17,7 +17,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! zng = { version = "0.12.8", features = ["view_prebuilt"] }
+//! zng = { version = "0.12.9", features = ["view_prebuilt"] }
 //! ```
 //!
 //! Then create your first app:
