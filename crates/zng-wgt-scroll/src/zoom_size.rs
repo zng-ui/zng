20a20d03f3cc41e5045d7c2209afd52146472d09
Samuel <sam.rodr.g@gmail.com>
2025-01-25 15:40:39 -0300

    Impl enabled

--- crates/zng-wgt-scroll/src/zoom_size.rs
+++ crates/zng-wgt-scroll/src/zoom_size.rs
@@ -9,24 +9,44 @@ use crate::SCROLL_SCALE_VAR;
 pub fn zoom_size_only(child: impl UiNode, enabled: impl IntoVar<bool>) -> impl UiNode {
     let enabled = enabled.into_var();
     let mut scale = 1.fct();
+    let mut _zoom_sub = VarHandle::dummy();
     match_node(child, move |c, op| match op {
         UiNodeOp::Init => {
-            WIDGET.sub_var_layout(&enabled).sub_var_layout(&SCROLL_SCALE_VAR);
+            WIDGET.sub_var(&enabled);
+            if enabled.get() {
+                _zoom_sub = SCROLL_SCALE_VAR.subscribe(UpdateOp::Layout, WIDGET.id());
+            }
+        }
+        UiNodeOp::Deinit => {
+            _zoom_sub = VarHandle::dummy();
         }
         UiNodeOp::Measure { wm, .. } => {
             wm.disable_inline();
         }
+        UiNodeOp::Update { .. } => {
+            if let Some(e) = enabled.get_new() {
+                if e {
+                    _zoom_sub = SCROLL_SCALE_VAR.subscribe(UpdateOp::Layout, WIDGET.id());
+                } else {
+                    _zoom_sub = VarHandle::dummy();
+                    scale = 1.fct();
+                }
+                WIDGET.layout();
+            }
+        }
         UiNodeOp::Layout { wl, final_size } => {
-            let s = SCROLL_SCALE_VAR.get();
-            if s != 1.fct() {
-                // return the unscaled size to not affect the parent layout,
-                // ideally the scaled parent will fit around the resized child.
-                *final_size = c.measure(&mut wl.to_measure(None));
-                let scaled_size = *final_size * s;
-                LAYOUT.with_constraints(PxConstraints2d::new_exact_size(scaled_size), || c.layout(wl));
-                if scale != s {
-                    scale = s;
-                    WIDGET.render_update();
+            if enabled.get() {
+                let s = SCROLL_SCALE_VAR.get();
+                if s != 1.fct() {
+                    // return the unscaled size to not affect the parent layout,
+                    // ideally the scaled parent will fit around the resized child.
+                    *final_size = c.measure(&mut wl.to_measure(None));
+                    let scaled_size = *final_size * s;
+                    LAYOUT.with_constraints(PxConstraints2d::new_exact_size(scaled_size), || c.layout(wl));
+                    if scale != s {
+                        scale = s;
+                        WIDGET.render_update();
+                    }
                 }
             }
         }
