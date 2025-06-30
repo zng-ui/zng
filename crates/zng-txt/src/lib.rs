f503c76427b70fd7c9c74ef1e8d00dcf630d068a
Samuel Guerra <sam.rodr.g@gmail.com>
2025-06-30 15:16:33 -0300

    Optimize `Txt::pop` transition to empty

--- crates/zng-txt/src/lib.rs
+++ crates/zng-txt/src/lib.rs
@@ -271,7 +271,7 @@ impl Txt {
             TxtData::Inline(d) => {
                 let s = inline_to_str(d);
                 if let Some((i, c)) = s.char_indices().last() {
-                    if !s.is_empty() {
+                    if i > 0 {
                         *d = str_to_inline(&s[..i]);
                     } else {
                         self.0 = TxtData::Static("");
