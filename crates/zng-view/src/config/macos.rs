2a9f804e78113865a863e43ee29333e41a784f4f
Samuel <sam.rodr.g@gmail.com>
2025-07-09 23:17:55 -0300

    Remove warnings about `touch_config` not being implemented in Linux, macOS and Windows (#684)
    These OS do not provide system wide touch settings.

--- crates/zng-view/src/config/macos.rs
+++ crates/zng-view/src/config/macos.rs
@@ -27,7 +27,8 @@ pub fn key_repeat_config() -> KeyRepeatConfig {
 }
 
 pub fn touch_config() -> TouchConfig {
-    super::other::touch_config()
+    // macOS does not provide touch config
+    TouchConfig::default()
 }
 
 pub fn colors_config() -> ColorsConfig {
