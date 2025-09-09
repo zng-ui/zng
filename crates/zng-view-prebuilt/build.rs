8991a646186344ab83814a905ce432d5b0ba4b17
Samuel Guerra <sam.rodr.g@gmail.com>
2025-09-09 15:33:37 -0300

    Remove  `fxhash` dependency
    Still depends on webrender, need to patch that.
    
    Added the advisory to ignore list, not even Mozilla is rushing to fix this, there is no actual vulnerability.

--- crates/zng-view-prebuilt/build.rs
+++ crates/zng-view-prebuilt/build.rs
@@ -1,11 +1,9 @@
 use std::{
     env,
-    hash::Hasher,
     path::{Path, PathBuf},
 };
 
-use base64::Engine;
-use hashers::jenkins::spooky_hash::SpookyHasher;
+use sha2::Digest;
 
 fn main() {
     println!("cargo::rustc-check-cfg=cfg(zng_lib_embedded)");
@@ -110,17 +108,10 @@ fn main() {
         let lib_bytes = std::fs::read(lib).unwrap();
 
         // just to identify build.
-        let mut hasher = SpookyHasher::new(u64::from_le_bytes(*b"prebuild"), u64::from_le_bytes(*b"view-lib"));
-        hasher.write(&lib_bytes);
-        let (a, b) = hasher.finish128();
-        let mut hash = [0; 16];
-        hash[..8].copy_from_slice(&a.to_le_bytes());
-        hash[8..].copy_from_slice(&b.to_le_bytes());
-
-        println!(
-            "cargo:rustc-env=ZNG_VIEW_LIB_HASH={}",
-            base64::engine::general_purpose::URL_SAFE_NO_PAD.encode(hash)
-        );
+        let mut hasher = sha2::Sha256::new();
+        hasher.update(&lib_bytes);
+        let hash = hasher.finalize();
+        println!("cargo:rustc-env=ZNG_VIEW_LIB_HASH={hash:x}",);
     } else if is_docs_rs || PathBuf::from("../../tools/cargo-do").exists() {
         println!("cargo:warning=view prebuilt not embedded, missing '{file}', call `do prebuild`");
     } else {
