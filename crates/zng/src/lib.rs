6178aaf1d6840218acab06e2681ba928517d900f
Samuel Guerra <sam.rodr.g@gmail.com>
2025-09-18 16:23:05 -0300

    Bump patch

--- crates/zng/src/lib.rs
+++ crates/zng/src/lib.rs
@@ -17,7 +17,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! zng = { version = "0.16.5", features = ["view_prebuilt"] }
+//! zng = { version = "0.16.6", features = ["view_prebuilt"] }
 //! ```
 //!
 //! Then create your first app:
