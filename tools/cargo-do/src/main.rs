8dd997cb499e71974acbfbc2bd2e3ef20209de01
Samuel <sam.rodr.g@gmail.com>
2025-09-22 16:49:40 -0300

    Add a footer note in the release message (#753)

--- tools/cargo-do/src/main.rs
+++ tools/cargo-do/src/main.rs
@@ -1235,7 +1235,15 @@ fn prebuild(mut args: Vec<&str>) {
 
     cmd(
         "cargo",
-        &["build", "-p", "zng-view", "--profile", profile, "--features", "ipc,software,bundle_licenses,image_all"],
+        &[
+            "build",
+            "-p",
+            "zng-view",
+            "--profile",
+            profile,
+            "--features",
+            "ipc,software,bundle_licenses,image_all",
+        ],
         &args,
     );
 
@@ -1809,6 +1817,9 @@ fn comment_feature(mut args: Vec<&str>) {
 
 // used by `workflows/release.yml`
 fn latest_release_changes(args: Vec<&str>) {
+    let output = args[0];
+    let github_action_id = args[1];
+
     let changelog = match std::fs::read_to_string("CHANGELOG.md") {
         Ok(c) => c,
         Err(e) => fatal(f!("failed to read CHANGELOG.md, {e}")),
@@ -1827,14 +1838,13 @@ fn latest_release_changes(args: Vec<&str>) {
             changes.push('\n');
         }
     }
-    let changes = changes.trim();
+    let _ = write!(
+        &mut changes,
+        "*Crates will be available on [crates.io](https://crates.io/crates/zng) once [Publish](https://github.com/zng-ui/zng/actions/runs/{github_action_id}) completes.*"
+    );
 
-    if let Some(out) = args.first() {
-        if let Err(e) = std::fs::write(out, changes.as_bytes()) {
-            fatal(f!("failed to write changes, {e}"));
-        }
-    } else {
-        println(changes)
+    if let Err(e) = std::fs::write(output, changes.as_bytes()) {
+        fatal(f!("failed to write changes, {e}"));
     }
 }
 
