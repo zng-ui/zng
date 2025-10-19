d8fda36449ce62bcb9c7ac4dbf65bdbb6269598e
Samuel <sam.rodr.g@gmail.com>
2025-10-19 01:31:04 -0300

    Release 0.18.2 (#804)

--- crates/zng-view/src/lib.rs
+++ crates/zng-view/src/lib.rs
@@ -11,8 +11,8 @@
 //!
 //! ```toml
 //! [dependencies]
-//! zng = "0.18.1"
-//! zng-view = "0.13.1"
+//! zng = "0.18.2"
+//! zng-view = "0.13.2"
 //! ```
 //!
 //! Then call `zng::env::init` before any other code in `main` to setup a view-process that uses
