9fbfa7f67818108be3cda8ec0235fd3ad134664a
Samuel Guerra <sam.rodr.g@gmail.com>
2025-11-02 15:08:30 -0300

    fmt

--- crates/zng-wgt-inspector/src/live/data_model.rs
+++ crates/zng-wgt-inspector/src/live/data_model.rs
@@ -1,13 +1,13 @@
 use std::{collections::HashMap, fmt, ops, sync::Arc};
 
 use parking_lot::Mutex;
+use std::fmt::Write as _;
 use zng_app::widget::{
     builder::WidgetType,
     info::WidgetInfoTree,
     inspector::{InspectorInfo, WidgetInfoInspectorExt},
 };
 use zng_layout::context::LayoutMask;
-use std::fmt::Write as _;
 use zng_view_api::window::FrameId;
 use zng_wgt::prelude::*;
 
@@ -391,12 +391,14 @@ impl INSPECTOR {
                 formatx!("{:.0?}", Size::from(size))
             }),
         );
-        builder.insert("metrics_used", target.render_watcher(|i| {
-            let i = i.bounds_info();
-            if let Some(m) = i.metrics() {
-                let mut out = String::new();
-                let used = i.metrics_used();
-                macro_rules! w {
+        builder.insert(
+            "metrics_used",
+            target.render_watcher(|i| {
+                let i = i.bounds_info();
+                if let Some(m) = i.metrics() {
+                    let mut out = String::new();
+                    let used = i.metrics_used();
+                    macro_rules! w {
                     ($($MASK:ident, $field:ident;)+) => {$(
                         #[allow(unused_assignments)]
                         if used.contains(LayoutMask::$MASK) {
@@ -404,23 +406,24 @@ impl INSPECTOR {
                         }
                     )+};
                 }
-                w! {
-                    CONSTRAINTS, constraints;
-                    CONSTRAINTS, inline_constraints;
-                    CONSTRAINTS, z_constraints;
-                    FONT_SIZE, font_size;
-                    ROOT_FONT_SIZE, root_font_size;
-                    SCALE_FACTOR, scale_factor;
-                    VIEWPORT, viewport;
-                    SCREEN_DENSITY, screen_density;
-                    DIRECTION, direction;
-                    LEFTOVER, leftover;
+                    w! {
+                        CONSTRAINTS, constraints;
+                        CONSTRAINTS, inline_constraints;
+                        CONSTRAINTS, z_constraints;
+                        FONT_SIZE, font_size;
+                        ROOT_FONT_SIZE, root_font_size;
+                        SCALE_FACTOR, scale_factor;
+                        VIEWPORT, viewport;
+                        SCREEN_DENSITY, screen_density;
+                        DIRECTION, direction;
+                        LEFTOVER, leftover;
+                    }
+                    out.into()
+                } else {
+                    Txt::default()
                 }
-                out.into()
-            } else {
-                Txt::default()
-            }
-        }));
+            }),
+        );
 
         if target.info().with(|i| i.parent().is_none()) {
             // root widget
