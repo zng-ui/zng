645689c95a14e848e7e79ef80d0d35a92961874d
Samuel <sam.rodr.g@gmail.com>
2025-11-09 23:36:26 -0300

    Release 0.19.2 (#838)

--- crates/zng-view/src/lib.rs
+++ crates/zng-view/src/lib.rs
@@ -11,8 +11,8 @@
 //!
 //! ```toml
 //! [dependencies]
-//! zng = "0.19.1"
-//! zng-view = "0.14.1"
+//! zng = "0.19.2"
+//! zng-view = "0.14.2"
 //! ```
 //!
 //! Then call `zng::env::init` before any other code in `main` to setup a view-process that uses
--- crates/zng/src/icon.rs
+++ crates/zng/src/icon.rs
@@ -75,7 +75,7 @@ pub use zng_wgt_text::icon::{GlyphIcon, GlyphSource, Icon, ico_color, ico_size};
 /// [Material Design Icons]: https://github.com/google/material-design-icons
 ///
 /// ```toml
-/// zng = { version = "0.19.1", features = ["material_icons"] }
+/// zng = { version = "0.19.2", features = ["material_icons"] }
 /// ```
 ///
 /// Handlers are registered for [`ICONS`] that provides the icons, the raw codepoints and glyph icon metadata is available in each font module.
