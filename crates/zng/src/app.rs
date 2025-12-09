1ef1deacd578f9ef08d04ffe3df84987f1cf491a
Samuel <sam.rodr.g@gmail.com>
2025-12-09 15:15:34 -0300

    Release 0.20.1 (#858)

--- crates/zng/src/app.rs
+++ crates/zng/src/app.rs
@@ -23,7 +23,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! zng = { version = "0.20.0", features = ["view_prebuilt"] }
+//! zng = { version = "0.20.1", features = ["view_prebuilt"] }
 //! ```
 //!
 //! ```no_run
