79660a0704f8c1812f2b638d1b4682dd0d8723da
Samuel Guerra <sam.rodr.g@gmail.com>
2026-03-13 23:30:16 -0300

    Release 0.21.5

--- crates/zng-view/src/lib.rs
+++ crates/zng-view/src/lib.rs
@@ -11,8 +11,8 @@
 //!
 //! ```toml
 //! [dependencies]
-//! zng = "0.21.4"
-//! zng-view = "0.16.4"
+//! zng = "0.21.5"
+//! zng-view = "0.16.5"
 //! ```
 //!
 //! Then call `zng::env::init` before any other code in `main` to setup a view-process that uses
--- crates/zng/src/lib.rs
+++ crates/zng/src/lib.rs
@@ -17,7 +17,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! zng = { version = "0.21.4", features = ["view_prebuilt"] }
+//! zng = { version = "0.21.5", features = ["view_prebuilt"] }
 //! ```
 //!
 //! Then create your first app:
