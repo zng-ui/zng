95b7162b9573895428c0d4610d7b592d69782e97
Samuel Guerra <sam.rodr.g@gmail.com>
2026-04-20 13:37:17 -0300

    Suppress "Cropping texture upload Box2D((0, 0), (0, 1)) to None"

--- crates/zng-app/src/lib.rs
+++ crates/zng-app/src/lib.rs
@@ -730,7 +730,8 @@ pub fn print_tracing_filter(level: &tracing::Level, metadata: &tracing::Metadata
 
     if metadata.level() == &tracing::Level::INFO && level < &tracing::Level::TRACE {
         // suppress large info about texture cache
-        if metadata.target().ends_with("webrender::device::gl") { // ends with to also support zng_webrender*
+        if metadata.target().ends_with("webrender::device::gl") {
+            // ends with to also support zng_webrender*
             return false;
         }
         // suppress config dump
@@ -743,7 +744,7 @@ pub fn print_tracing_filter(level: &tracing::Level, metadata: &tracing::Metadata
         if metadata.target().ends_with("webrender::device::gl") {
             // Suppress "Cropping texture upload Box2D((0, 0), (0, 1)) to None"
             // This happens when an empty frame is rendered.
-            if metadata.line() == Some(4647) {
+            if metadata.line() == Some(4652) {
                 return false;
             }
         }
