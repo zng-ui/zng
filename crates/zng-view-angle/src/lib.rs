4dfc535efe93c791113c95d53c270edf52304744
Samuel Guerra <sam.rodr.g@gmail.com>
2026-05-17 23:50:29 -0300

    Add license.

--- crates/zng-view-angle/src/lib.rs
+++ crates/zng-view-angle/src/lib.rs
@@ -15,14 +15,14 @@
 //! zng-view-angle = { version = "0.1.0", features = ["download"] }
 //! ```
 //!
-//! With the `"download"` feature enabled the crate will download the required ANGLE DLLs from [`zng-ui/build-angle`]
+//! With the `"download"` feature enabled the crate will download the required ANGLE DLLs from [zng-ui/build-angle]
 //! and copy them to the output dir. Its only two files `libEGL.dll` and `libGLESv2.dll`, around 5MB total, you must
 //! ensure these files are packaged with the app installer.
 //!
 //! **Second** signal the view-process to use ANGLE:
 //!
 //! ```
-//! # macro_rules! { () => {
+//! # macro_rules! demo { () => {
 //! use zng::prelude::*;
 //!
 //! fn main() {
@@ -30,7 +30,10 @@
 //!
 //!     APP.defaults().run_window(async {
 //!         #[cfg(windows)]
-//!         zng_view_angle::register_root_extender();
+//!         {
+//!             zng_view_angle::register_license();
+//!             zng_view_angle::register_root_extender();
+//!         }
 //!
 //!         Window! {
 //!             // ...
@@ -43,13 +46,18 @@
 //! The [`register_root_extender`] will insert a node in all subsequent windows that signals the view-process to
 //! find and use the ANGLE DLLs when render mode uses the GPU. If the DLLs are not found the native OpenGL driver is used instead.
 //!
-//! [`zng-ui/build-angle`]: https://github.com/zng-ui/build-angle
+//! The [`register_license`] simply adds the ANGLE BSD3-Clause license to the [`LICENSES`] service.
+//!
+//! [zng-ui/build-angle]: https://github.com/zng-ui/build-angle
 //!
 //! # Crate
 //!
 #![doc = include_str!(concat!("../", std::env!("CARGO_PKG_README")))]
 
-use zng_app::view_process::{VIEW_PROCESS, VIEW_PROCESS_INITED_EVENT};
+use zng_app::{
+    third_party::{LICENSES, License, LicenseUsed, User},
+    view_process::{VIEW_PROCESS, VIEW_PROCESS_INITED_EVENT},
+};
 use zng_ext_window::{WINDOW_Ext as _, WINDOWS_EXTENSIONS};
 use zng_view_api::api_extension::*;
 use zng_wgt::prelude::*;
@@ -110,3 +118,55 @@ pub fn prefer_angle_egl(child: impl IntoUiNode, enable: impl IntoValue<bool>) ->
 pub fn register_root_extender() {
     WINDOWS_EXTENSIONS.register_root_extender(|a| prefer_angle_egl(a.root, true));
 }
+
+/// Register the ANGLE license with the [`LICENSES`] service.
+///
+/// The license is a [BSD3-Clause type license](https://github.com/google/angle/blob/fca8ca8a87c387515e0d2901916e4fae6b97e83f/LICENSE).
+pub fn register_license() {
+    fn l() -> Vec<LicenseUsed> {
+        vec![LicenseUsed {
+            license: License::new("BSD3-Clause", r#"BSD 3-Clause "Revised" License"#, LICENSE),
+            used_by: vec![User::new(
+                std::env!("CARGO_PKG_NAME"),
+                std::env!("CARGO_PKG_VERSION"),
+                "https://github.com/zng-ui/zng",
+            )],
+        }]
+    }
+    LICENSES.register(l);
+}
+
+const LICENSE: &str = r#"
+// Copyright 2018 The ANGLE Project Authors.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//     Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//     Redistributions in binary form must reproduce the above
+//     copyright notice, this list of conditions and the following
+//     disclaimer in the documentation and/or other materials provided
+//     with the distribution.
+//
+//     Neither the name of TransGaming Inc., Google Inc., 3DLabs Inc.
+//     Ltd., nor the names of their contributors may be used to endorse
+//     or promote products derived from this software without specific
+//     prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+"#;
